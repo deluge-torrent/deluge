64d21484ef2f7ef957e724068d38ad8b2a12606f
Alon Zakai <kripkensteiner@gmail.com>
2006-12-02 19:18:28 +0000

    manager ideas in comment

--- library/pytorrent_manager.py
+++ library/pytorrent_manager.py
@@ -1,7 +1,6 @@
 # 
-# Copyright (c) 2006 Alon Zakai ('Kripken') <kripkensteiner@gmail.com>
-#
-# 2006-15-9
+# Copyright (C) 2006 Zach Tibbitts <zach@collegegeek.org>
+# Copyright (C) 2006 Alon Zakai ('Kripken') <kripkensteiner@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,7 +21,25 @@
 # pytorrent module. pytorrent itself is mainly an interface to libtorrent,
 # with some arrangements of exception classes for Python, etc.; also, some
 # additional code that fits in well at the C++ level of libtorrent. All other
-# backend routines should be in pytorrent-manager. (Just an idea)
+# backend routines should be in pytorrent-manager.
+#
+# Things which pytorrent-manager should do:
+#
+# 1. Save/Load torrent states (list of torrents in system, + their states) to file
+#    (AutoSaveTorrents in deluge.py)
+# 2. Manage basic queuing: how many active downloads, and autopause the rest (this
+#    is currently spread along deluge.py and torrenthandler.py)
+# 2a.Queue up and queue down, etc., functions (in deluge.py)
+# 3. Save/Load a preferences file, with all settings (max ports, listen port, use
+#    DHT, etc. etc.)
+# 4. Manage autoseeding to a certain share % (currently in torrenthandler.py)
+# 5. Handle caching of .torrent files and so forth (currently in deluge.py)
+# 6. A 'clear completed' function, that works on the BACKEND data, unlike the
+#    current implementation which works on the frontend (in torrenthander.py)
+# 7. Various statistics-reporting functions - # of active torrents, etc. etc.
+#    (getNumActiveTorrents in torrenthandler.py)
+# 8. Remove torrent's data (in deluge.py)
+#
 
 import pytorrent
 
