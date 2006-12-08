1ae9201eb6e597015f906d3f16ecfbd23825d8db
Zach Tibbitts <zach@collegegeek.org>
2006-12-08 19:06:49 +0000

    -

--- dcommon.py
+++ dcommon.py
@@ -1,8 +1,24 @@
-#!/usr/bin/env python2.4
+# dcommon.py
 #
-# Deluge common class
-# For functions and variables that
-#  need to be accessed globally.
+# Copyright (C) Zach Tibbitts 2006 <zach@collegegeek.org>
+# 
+# Deluge is free software.
+# 
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option)
+# any later version.
+# 
+# dcommon.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with main.py.  If not, write to:
+# 	The Free Software Foundation, Inc.,
+# 	51 Franklin Street, Fifth Floor
+# 	Boston, MA  02110-1301, USA.
 
 import sys, os, webbrowser
 
