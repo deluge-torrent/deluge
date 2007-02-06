a85fcc452a18a48b782c302df46a35be415d29be
Zach Tibbitts <zach@collegegeek.org>
2007-02-06 22:59:41 +0000

    new run svn script

--- run_svn.sh
+++ run_svn.sh
@@ -4,13 +4,16 @@ echo "  Updating to latest SVN version  "
 echo "*******************************************"
 svn update
 echo "*******************************************"
+echo "  Linking modules to working directory  "
+echo "*******************************************"
+ln -s src/*.py .
+echo "*******************************************"
 echo "  Building Deluge-SVN  "
 echo "*******************************************"
 python setup.py build
 echo "*******************************************"
 echo "  Linking C++ module to current directory  "
 echo "*******************************************"
-rm deluge_core.so
 ln -s build/lib*/deluge_core.so .
 echo "*******************************************"
 echo "  Launching Deluge-SVN  "
