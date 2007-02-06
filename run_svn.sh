156fc98db1973100679b667f0c03761f22de78f8
Zach Tibbitts <zach@collegegeek.org>
2007-02-06 23:58:03 +0000

    new svn script

--- run_svn.sh
+++ run_svn.sh
@@ -1,12 +1,40 @@
 #!/bin/sh
+#
+# This file is public domain
+# 
+# run_svn.sh
+#
+# This is a small shell script to build and run
+# a working copy of Deluge without installing
+# it globally.  This is only intended for
+# testing SVN versions of Deluge, and should 
+# not be used for a final installation.
+#
+# I make no guarantees that this script will 
+# work for you, but it does build and run
+# properly on my system.
+#
+
+if [ -d ./working/ ]
+then
+	echo "*******************************************"
+	echo "  Removing old files  "
+	echo "*******************************************"
+	rm -rf working/
+fi
 echo "*******************************************"
 echo "  Updating to latest SVN version  "
 echo "*******************************************"
 svn update
 echo "*******************************************"
-echo "  Linking modules to working directory  "
+echo "  Copy source to working directory  "
 echo "*******************************************"
-ln -s src/*.py .
+cp -r src working
+echo "*******************************************"
+echo "  Linking files to working directory  "
+echo "*******************************************"
+ln -s ../glade working/
+ln -s ../pixmaps working/
 echo "*******************************************"
 echo "  Building Deluge-SVN  "
 echo "*******************************************"
@@ -14,8 +42,14 @@ python setup.py build
 echo "*******************************************"
 echo "  Linking C++ module to current directory  "
 echo "*******************************************"
-ln -s build/lib*/deluge_core.so .
+for file in $(echo build/*)
+do
+	if [ -e $file/deluge/deluge_core.so ]
+	then
+		ln -s ../$file/deluge/deluge_core.so working/
+	fi
+done
 echo "*******************************************"
 echo "  Launching Deluge-SVN  "
 echo "*******************************************"
-python delugegtk.py
+python working/delugegtk.py
