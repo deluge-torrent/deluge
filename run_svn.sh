967632cb84a5ff195e9f01155d0f18d86b882c2b
Zach Tibbitts <zach@collegegeek.org>
2007-02-07 00:11:15 +0000

    updated run_svn.sh script

--- run_svn.sh
+++ run_svn.sh
@@ -5,51 +5,41 @@
 # run_svn.sh
 #
 # This is a small shell script to build and run
-# a working copy of Deluge without installing
-# it globally.  This is only intended for
-# testing SVN versions of Deluge, and should 
-# not be used for a final installation.
-#
-# I make no guarantees that this script will 
-# work for you, but it does build and run
-# properly on my system.
+# a working copy of Deluge-SVN
 #
 
 if [ -d ./working/ ]
 then
-	echo "*******************************************"
-	echo "  Removing old files  "
-	echo "*******************************************"
-	rm -rf working/
+	echo "  Removing old files:  "
+	rm -rvf working
 fi
-echo "*******************************************"
-echo "  Updating to latest SVN version  "
-echo "*******************************************"
+echo "  Creating new working directory:  "
+mkdir -v working/ working/glade/ working/pixmaps/
+echo "  Updating to latest SVN version:  "
 svn update
-echo "*******************************************"
-echo "  Copy source to working directory  "
-echo "*******************************************"
-cp -r src working
-echo "*******************************************"
-echo "  Linking files to working directory  "
-echo "*******************************************"
-ln -s ../glade working/
-ln -s ../pixmaps working/
-echo "*******************************************"
-echo "  Building Deluge-SVN  "
-echo "*******************************************"
+echo "  Copy source to working directory:  "
+for file in $(echo src/*)
+do
+	cp -v $file working/
+done
+echo "  Copying data to working directory:  "
+for file in $(echo glade/*)
+do
+	cp -v $file working/glade/
+done
+for file in $(echo pixmaps/*)
+do
+	cp -v $file working/pixmaps/
+done
+echo "  Building Deluge-SVN:  "
 python setup.py build
-echo "*******************************************"
-echo "  Linking C++ module to current directory  "
-echo "*******************************************"
+echo "  Copying C++ module to current directory:  "
 for file in $(echo build/*)
 do
 	if [ -e $file/deluge/deluge_core.so ]
 	then
-		ln -s ../$file/deluge/deluge_core.so working/
+		cp -v $file/deluge/deluge_core.so working/
 	fi
 done
-echo "*******************************************"
-echo "  Launching Deluge-SVN  "
-echo "*******************************************"
+echo "  Launching Deluge-SVN:  "
 python working/delugegtk.py
