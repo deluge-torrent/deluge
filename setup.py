45f5b0427ac4e56cb791aec4a030d1bb0a9f40a8
Zach Tibbitts <zach@collegegeek.org>
2007-06-11 22:35:20 +0000

    note for packagers

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ python_version = platform.python_version()[0:3]
 
 NAME		= "deluge"
 FULLNAME	= "Deluge BitTorrent Client"
-VERSION		= "0.5.0.99.2"
+VERSION		= "0.5.1"
 AUTHOR		= "Zach Tibbitts, Alon Zakai, Marcos Pinto"
 EMAIL		= "zach@collegegeek.org, kripkensteiner@gmail.com, marcospinto@dipconsultants.com"
 DESCRIPTION	= "A bittorrent client written in PyGTK"
