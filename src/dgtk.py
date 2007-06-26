44d649036b1f49eecad20656fb42196f706ab6f6
Andrew Resch <andrewresch@gmail.com>
2007-06-26 07:30:53 +0000

    Changed the min_width of the torrent view columns so they can be shrunk smaller than previously able to.

--- src/dgtk.py
+++ src/dgtk.py
@@ -67,6 +67,7 @@ def add_func_column(view, header, func, data, sortid=None):
 			column.set_sort_column_id(data)
 	column.set_resizable(True)
 	column.set_expand(False)
+	column.set_min_width(10)
 	view.append_column(column)
 	return column
 	
@@ -78,6 +79,7 @@ def add_text_column(view, header, cid):
 	column.set_sort_column_id(cid)
 	column.set_resizable(True)
 	column.set_expand(False)
+	column.set_min_width(10)
 	view.append_column(column)
 	return column
 
@@ -88,6 +90,7 @@ def add_progress_column(view, header, pid, mid):
 	column.set_sort_column_id(pid)
 	column.set_resizable(True)
 	column.set_expand(False)
+	column.set_min_width(10)
 	view.append_column(column)
 	return column
 
@@ -98,6 +101,7 @@ def add_toggle_column(view, header, cid, toggled_signal=None):
 	column.set_clickable(True)
 	column.set_resizable(True)
 	column.set_expand(False)
+	column.set_min_width(10)
 	view.append_column(column)
 	if toggled_signal is not None:
 		render.connect("toggled", toggled_signal)
@@ -108,6 +112,7 @@ def add_texticon_column(view, header, icon_col, text_col):
 	column.set_clickable(True)
 	column.set_resizable(True)
 	column.set_expand(False)
+	column.set_min_width(10)
 	render = gtk.CellRendererPixbuf()
 	column.pack_start(render, expand=False)
 	column.add_attribute(render, 'pixbuf', icon_col)
