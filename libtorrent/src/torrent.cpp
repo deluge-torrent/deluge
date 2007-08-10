069ebf02287934fc4ed029d3c1be9b90d871160d
Alex Dedul <rotmer@gmail.com>
2007-08-10 01:15:48 +0000

    Locally patched libtorrent to not reset per torrent preferences on each resume(). See ticket #118 in libtorrent's trac.

--- libtorrent/src/torrent.cpp
+++ libtorrent/src/torrent.cpp
@@ -2527,10 +2527,6 @@ namespace libtorrent
 #endif
 
 		m_paused = false;
-		m_uploads_quota.min = 0;
-		m_connections_quota.min = 2;
-		m_uploads_quota.max = std::numeric_limits<int>::max();
-		m_connections_quota.max = std::numeric_limits<int>::max();
 
 		// tell the tracker that we're back
 		m_event = tracker_request::started;
