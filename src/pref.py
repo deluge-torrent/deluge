c29a303dd46d49947fe7909929c237822189507d
Marcos Pinto <markybob@dipconsultants.com>
2007-08-10 21:21:58 +0000

    fix switch

--- src/pref.py
+++ src/pref.py
@@ -114,7 +114,7 @@ DEFAULT_PREFS = {
     "tray_downloadspeedlist" : [5.0, 10.0, 30.0, 80.0, 300.0],
     "tray_passwd" : "",
     "tray_uploadspeedlist" : [5.0, 10.0, 30.0, 80.0, 300.0],
-    "use_full_storage" : True,
+    "use_compact_storage" : True,
     "use_default_dir" : False,
     "use_natpmp" : False,
     "use_upnp" : False,
