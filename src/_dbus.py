40f9b0a7d00258aeb3e9673f2ee44671eaa93588
Alex Dedul <rotmer@gmail.com>
2007-08-17 09:01:26 +0000

    Sync _dbus.py from deluge-plisk branch.

--- src/_dbus.py
+++ src/_dbus.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 #
+# Copyright (C) Alex Dedul 2007 <rotmer@gmail.com>
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2, or (at your option)
