1ac7e0eaab4b9637ea669dd74eb6c2e7efff46f6
Marcos Pinto <markybob@dipconsultants.com>
2007-09-04 16:40:40 +0000

    prio range fix from andar

--- src/deluge_core.cpp
+++ src/deluge_core.cpp
@@ -1827,7 +1827,7 @@ static PyObject *torrent_prioritize_first_last_pieces(PyObject *self,
         }        
             
         int start_piece = tor_info.map_file(i, 0, 0).piece;
-        int end_piece = tor_info.map_file(i, file.size, 0).piece;
+        int end_piece = tor_info.map_file(i, file.size, 0).piece -1;
         // Set prio_size to 1% of the file size
         size_type prio_size = file.size / 100;
         int prio_pieces = tor_info.map_file(i, prio_size, 0).piece -
