af4c9c0ea81733e3128b08f007e8c1487f2e5474
Andrew Resch <andrewresch@gmail.com>
2007-09-09 12:07:20 +0000

    Updated Scheduler plugin to work with current trunk.

---
+++ plugins/Scheduler/__init__.py
@@ -0,0 +1,17 @@
+# An example plugin for use with Deluge
+
+plugin_name = "Scheduler"            # The name of the plugin
+plugin_author = "Lazka - updated by markybob"           # The author's Name
+plugin_version = "0.5.6"                  # The plugin's version number
+plugin_description = "Scheduler"  # A description of the plugin
+
+def deluge_init(deluge_path):
+    global path
+    path = deluge_path
+
+
+from Scheduler.plugin import plugin_Scheduler
+
+def enable(core, interface):
+    global path
+    return plugin_Scheduler(path, core, interface)
