20c22477f4c09bc394fa249ac57bacb165201bc2
Andrew Resch <andrewresch@gmail.com>
2007-09-09 12:04:17 +0000

    Updated Scheduler plugin so that it works with current trunk.

--- plugins/Scheduler/plugin.py
+++ plugins/Scheduler/plugin.py
@@ -31,7 +31,6 @@ class plugin_Scheduler:
 
 	def update(self):
 		time_now = time.localtime(time.time())
-		
 		if self.status is not self.button_state[time_now[3]][time_now[6]]:
 			self.status = self.button_state[time_now[3]][time_now[6]]
 
@@ -55,14 +54,14 @@ class plugin_Scheduler:
 		self.core.apply_queue()
 
 	def limit(self):
-		self.config.set("max_download_rate", float(self.dllimit))
-		self.config.set("max_upload_rate", float(self.ullimit))
+		self.config.set("max_download_speed", float(self.dllimit))
+		self.config.set("max_upload_speed", float(self.ullimit))
 
 	def unlimit(self):
 		self.interface.apply_prefs()
 
 	#Configuration dialog
-	def configure(self):
+	def configure(self, window):
 		global scheduler_select
 
 		self.button_state_temp = copy.deepcopy(self.button_state)
