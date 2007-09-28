72d868ff796ece860d1b45738b7ac9c9f930ba82
Zach Tibbitts <zach@collegegeek.org>
2007-09-28 01:47:12 +0000

    WebUi Plugin - rev40

---
+++ plugins/WebUi/__init__.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+# Dbus Ipc for experimental web interface
+#
+# dbus_interface.py
+#
+# Copyright (C) Martijn Voncken 2007 <mvoncken@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, write to:
+#     The Free Software Foundation, Inc.,
+#     51 Franklin Street, Fifth Floor
+#     Boston, MA  02110-1301, USA.
+#
+#  In addition, as a special exception, the copyright holders give
+#  permission to link the code of portions of this program with the OpenSSL
+#  library.
+#  You must obey the GNU General Public License in all respects for all of
+#  the code used other than OpenSSL. If you modify file(s) with this
+#  exception, you may extend this exception to your version of the file(s),
+#  but you are not obligated to do so. If you do not wish to do so, delete
+#  this exception statement from your version. If you delete this exception
+
+plugin_name = "Web User interface"
+plugin_author = "Martijn Voncken"
+plugin_version = "rev."
+plugin_description = """A Web based User Interface (and dbus-ipc)
+beta test version, disclaimer, etc..
+"""
+
+import deluge.common, deluge.pref
+from dbus_interface import DbusManager
+import gtk
+import os
+
+from subprocess import Popen
+
+plugin_version += open(os.path.join(os.path.dirname(__file__),'revno')).read()
+
+def deluge_init(deluge_path):
+    global path
+    path = deluge_path
+
+def enable(core, interface):
+    global path
+    return plugin_WebUi(path, core, interface)
+
+class plugin_WebUi:
+    def __init__(self, path, deluge_core, deluge_interface):
+        self.path = path
+        self.core = deluge_core
+        self.interface = deluge_interface
+        self.proc = None
+
+        self.config_file = deluge.common.CONFIG_DIR + "/webui.conf"
+        self.config = deluge.pref.Preferences(self.config_file, False)
+        try:
+            self.config.load()
+        except IOError:
+            # File does not exist
+            pass
+
+        if not self.config.get('port'): #ugly way to detect new config file.
+            #set default values:
+            self.config.set("port", 8112)
+            self.config.set("user", "deluge")
+            self.config.set("pwd", "deluge")
+            #future->use deluge-core setting for download_dir (if it is set)
+            self.config.set("download_dir", os.path.expanduser("~/"))
+            self.config.set("torrent_dir", os.path.expanduser("~/"))
+            self.config.set("auto_refresh", False)
+            self.config.set("auto_refresh_secs", 4)
+            self.config.set("template", "deluge")
+            self.config.save(self.config_file)
+
+
+        self.dbusManager = DbusManager(deluge_core, deluge_interface
+            , self.config, self.config_file)
+        self.start_server()
+
+    def unload(self):
+        print 'WebUI:unload..'
+        self.kill_server()
+        #self.dbusManager.
+
+    def update(self):
+        pass
+
+    ## This will be only called if your plugin is configurable
+    def configure(self,parent_dialog):
+        d = ConfigDialog(self.config,self)
+        if d.run() == gtk.RESPONSE_OK:
+            d.save_config()
+        d.destroy()
+
+    def start_server(self):
+        self.kill_server()
+        print 'start Webui..'
+        path = os.path.dirname(__file__)
+        server_bin = path + '/run_webserver'
+        port = str(self.config.get('port'))
+        self.proc = Popen((server_bin, port),cwd=path)
+
+    def kill_server(self):
+        if self.proc:
+            print "webserver: kill %i"%self.proc.pid
+            os.system("kill %i"%self.proc.pid)
+        self.proc = None
+
+
+class ConfigDialog(gtk.Dialog):
+    """
+    sorry, can't get used to gui builders.
+    from what I read glade is better, but i dont want to invest time in them.
+    """
+    def __init__(self, config, plugin):
+        gtk.Dialog.__init__(self)
+        self.config = config
+        self.plugin = plugin
+        self.vb = gtk.VBox()
+        self.set_title(_("WebUi Config"))
+
+        template_path = os.path.join(os.path.dirname(__file__), 'templates')
+        self.templates = [dirname for dirname
+            in os.listdir(template_path)
+            if os.path.isdir(os.path.join(template_path, dirname))]
+
+        self.port = self.add_widget(_('Port Number'), gtk.SpinButton())
+        self.user = self.add_widget(_('User'), gtk.Entry())
+        self.pwd = self.add_widget(_('Password'), gtk.Entry())
+        self.template = self.add_widget(_('Template'), gtk.combo_box_new_text())
+        self.download_dir = self.add_widget(_('Download Directory'),
+            gtk.FileChooserButton(_('Download Directory')))
+        self.torrent_dir = self.add_widget(_('Torrent Directory'),
+            gtk.FileChooserButton(_('Torrent Directory')))
+
+        self.download_dir.set_action(gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER)
+        self.torrent_dir.set_action(gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER)
+        self.port.set_range(80, 65536)
+        self.port.set_increments(1, 10)
+        for item in self.templates:
+            self.template.append_text(item)
+        if not self.config.get("template") in self.templates:
+            self.config.set("template","deluge")
+
+        self.user.set_text(self.config.get("user"))
+        self.pwd.set_text(self.config.get("pwd"))
+        self.port.set_value(int(self.config.get("port")))
+        self.template.set_active(
+            self.templates.index(self.config.get("template")))
+        self.torrent_dir.set_filename(self.config.get("torrent_dir"))
+        self.download_dir.set_filename(self.config.get("download_dir"))
+        self.vbox.pack_start(self.vb, True, True, 0)
+        self.vb.show_all()
+
+        self.add_buttons(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL
+            ,gtk.STOCK_OK, gtk.RESPONSE_OK)
+
+    def add_widget(self,label,w=None):
+        hb = gtk.HBox()
+        lbl = gtk.Label(label)
+        lbl.set_size_request(200,20)
+        hb.pack_start(lbl,False,False, 0)
+        hb.pack_start(w,True,True, 0)
+
+        self.vb.pack_start(hb,False,False, 0)
+        return w
+        self.add_buttons(dgtk.STOCK_CLOSE, dgtk.RESPONSE_CLOSE)
+
+    def save_config(self):
+        print 'save config'
+        self.config.set("user", self.user.get_text())
+        self.config.set("pwd", self.pwd.get_text())
+        self.config.set("port", int(self.port.get_value()))
+        self.config.set("template", self.template.get_active_text())
+        self.config.set("torrent_dir", self.torrent_dir.get_filename())
+        self.config.set("download_dir",self.download_dir.get_filename())
+        self.config.save(self.plugin.config_file)
+        self.plugin.start_server() #restarts server
+
+
+
+
+
+
+
+
+
---
+++ plugins/WebUi/deluge_webserver.py
@@ -0,0 +1,391 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# deluge_webserver.py
+#
+# Copyright (C) Martijn Voncken 2007 <mvoncken@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, write to:
+#     The Free Software Foundation, Inc.,
+#     51 Franklin Street, Fifth Floor
+#     Boston, MA  02110-1301, USA.
+#
+#  In addition, as a special exception, the copyright holders give
+#  permission to link the code of portions of this program with the OpenSSL
+#  library.
+#  You must obey the GNU General Public License in all respects for all of
+#  the code used other than OpenSSL. If you modify file(s) with this
+#  exception, you may extend this exception to your version of the file(s),
+#  but you are not obligated to do so. If you do not wish to do so, delete
+#  this exception statement from your version. If you delete this exception
+#  statement from all source files in the program, then also delete it here.
+
+"""
+Todo's before beta:
+-alternating rows?
+-__init__:unload plugin is broken!
+-__init__:kill->restart is not waiting for kill to be finished.
+-redir is broken.
+--later/features:---
+-set prio
+-clear finished?
+-torrent files.
+"""
+import webpy022 as web
+from webpy022.webapi import cookies, setcookie
+from webpy022.http import seeother, url
+from webpy022.utils import Storage
+from webpy022 import template
+
+import dbus
+
+import gettext, os, platform, locale
+import random
+import base64
+from operator import attrgetter
+
+from deluge import common
+from deluge.common import INSTALL_PREFIX
+
+
+#init:
+APP = 'deluge'
+DIR = os.path.join(INSTALL_PREFIX, 'share', 'locale')
+if platform.system() != "Windows":
+    locale.setlocale(locale.LC_MESSAGES, '')
+    locale.bindtextdomain(APP, DIR)
+    locale.textdomain(APP)
+else:
+    locale.setlocale(locale.LC_ALL, '')
+gettext.bindtextdomain(APP, DIR)
+gettext.textdomain(APP)
+gettext.install(APP, DIR)
+
+random.seed()
+bus = dbus.SessionBus()
+proxy = bus.get_object("org.deluge_torrent.dbusplugin"
+    , "/org/deluge_torrent/DelugeDbusPlugin")
+
+web.webapi.internalerror = web.debugerror
+
+#/init
+
+#framework:
+SESSIONS = {}
+
+def do_redirect():
+    """for redirects after a POST"""
+    vars = web.input(redir = None)
+    ck = cookies()
+
+    if vars.redir:
+        seeother(vars.redir)
+    elif ("order" in ck and "sort" in ck):
+        seeother(url("/index", sort=ck['sort'], order=ck['order']))
+    else:
+        seeother(url("/index"))
+
+def deluge_page_noauth(func):
+    """
+    add http headers
+    print result of func
+    """
+    def deco(self, name=None):
+            web.header("Content-Type", "text/html; charset=utf-8")
+            web.header("Cache-Control", "no-cache, must-revalidate")
+            res = func(self, name)
+            print res
+    return deco
+
+def check_session(func):
+    """
+    a decorator
+    return func if session is valid, else redirect to login page.
+    """
+    def deco(self, name):
+        ck = cookies()
+        if ck.has_key("session_id") and ck["session_id"] in SESSIONS:
+            return func(self, name) #ok, continue..
+        else:
+            seeother("/login") #do not continue, and redirect to login page
+    return deco
+
+def deluge_page(func):
+    return check_session(deluge_page_noauth(func))
+
+def auto_refreshed(func):
+    "decorator:adds a refresh header"
+    def deco(self, name):
+        if proxy.get_webui_config('auto_refresh'):
+            web.header("Refresh", "%i ; url=%s" %
+                (proxy.get_webui_config('auto_refresh_secs'),
+                web.changequery()))
+        return func(self, name)
+    return deco
+
+def error_page(error):
+    web.header("Content-Type", "text/html; charset=utf-8")
+    web.header("Cache-Control", "no-cache, must-revalidate")
+    print render.error(error)
+
+#/framework
+
+#utils:
+torrent_keys = ['distributed_copies', 'download_payload_rate',
+    'download_rate', 'eta', 'is_seed', 'message', 'name', 'next_announce',
+    'num_files', 'num_peers', 'num_pieces', 'num_seeds', 'paused',
+    'piece_length','progress', 'ratio', 'total_done', 'total_download',
+    'total_payload_download', 'total_payload_upload', 'total_peers',
+    'total_seeds', 'total_size', 'total_upload', 'total_wanted',
+    'tracker_status', 'upload_payload_rate', 'upload_rate',
+    'uploaded_memory','tracker']
+
+def get_torrent_status(torrent_id):
+    """
+    helper method.
+    enhance proxy.get_torrent_status with some extra data
+    """
+    status = proxy.get_torrent_status(torrent_id,torrent_keys)
+    status["id"] = torrent_id
+
+    #for naming the status-images
+    status["calc_state_str"] = "downloading"
+    if status["paused"]:
+        status["calc_state_str"] = "inactive"
+    elif status["is_seed"]:
+        status["calc_state_str"] = "seeding"
+
+    #action for torrent_pause
+    if status["calc_state_str"] == "inactive":
+        status["action"] = "start"
+    else:
+        status["action"] = "stop"
+
+    #add some pre-calculated values
+    status.update({
+        "calc_total_downloaded"  : (common.fsize(status["total_done"])
+            + " (" + common.fsize(status["total_download"]) + ")"),
+        "calc_total_uploaded": (common.fsize(status['uploaded_memory']
+            + status["total_payload_upload"]) + " ("
+            + common.fsize(status["total_upload"]) + ")"),
+    })
+
+    return Storage(status) #Storage for easy templating.
+
+#/utils
+
+#template-defs:
+def template_crop(text, end):
+    if len(text) > end:
+        return text[0:end - 3] + '...'
+    return text
+
+def sort_head(id,name):
+    #got tired of doing these complex things inside templetor..
+    vars = web.input(sort=None, order=None)
+    active_up = False
+    active_down = False
+    order = 'down'
+
+    if vars.sort == id:
+        if vars.order == 'down':
+            order = 'up'
+            active_down = True
+        else:
+            active_up = True
+
+    return render.sort_column_head(id, name, order, active_up, active_down)
+
+
+render = template.render('templates/%s/' % proxy.get_webui_config('template'))
+
+template.Template.globals['crop']  = template_crop
+template.Template.globals['fspeed']  = common.fspeed
+template.Template.globals['fsize']  = common.fsize
+template.Template.globals['sorted']  = sorted
+template.Template.globals['_']  = _ #gettext/translations
+template.Template.globals['deluge_web_version']  = ('rev.'
+    + open(os.path.join(os.path.dirname(__file__),'revno')).read())
+template.Template.globals['render']  = render #for easy resuse of templates
+template.Template.globals['sort_head']  = sort_head
+template.Template.globals['get_config']  = proxy.get_webui_config
+template.Template.globals['self_url']  = web.changequery
+
+#/template-defs
+
+#routing:
+urls = (
+    "/login(.*)", "login",
+    "/index(.*)", "index",
+    "/torrent/info/(.*)", "torrent_info",
+    "/torrent/pause(.*)", "torrent_pause",
+    "/torrent/add(.*)", "torrent_add",
+    "/torrent/delete/(.*)", "torrent_delete",
+    "/pause_all(.*)", "pause_all",
+    "/resume_all(.*)", "resume_all",
+    "/refresh/set(.*)", "refresh_set",
+    "/refresh/(.*)", "refresh",
+    "/home(.*)", "home",
+    #default-pages
+    "/", "login",
+    "", "login"
+)
+
+#/routing
+
+#pages:
+class login:
+    @deluge_page_noauth
+    def GET(self, name):
+        return render.login()
+
+    def POST(self, name):
+        vars = web.input(var = None, pwd = None)
+
+        if (vars.user == proxy.get_webui_config('user')
+            and vars.pwd == proxy.get_webui_config('pwd')):
+            #start new session
+            session_id = str(random.random())
+            SESSIONS[session_id]  = {"user":vars.user}
+            setcookie("session_id", session_id)
+            do_redirect()
+        else:
+            error_page(_("Username or Password is invalid."))
+
+class home:
+    @check_session
+    def GET(self, name):
+        do_redirect()
+
+class index:
+    "page containing the torrent list."
+    @auto_refreshed
+    @deluge_page
+    def GET(self, name):
+        vars = web.input(sort=None, order=None)
+
+        status_rows = [get_torrent_status(torrent_id)
+            for torrent_id in proxy.get_torrent_state()]
+
+        #sorting:
+        if vars.sort:
+            status_rows.sort(key=attrgetter(vars.sort))
+            if vars.order == 'up':
+                status_rows = reversed(status_rows)
+
+            setcookie("order", vars.order)
+            setcookie("sort", vars.sort)
+
+        return render.index(status_rows)
+
+class torrent_info:
+    "torrent details"
+    @auto_refreshed
+    @deluge_page
+    def GET(self, torrent_id):
+        return render.torrent_info(get_torrent_status(torrent_id))
+
+class torrent_pause:
+    "start/stop a torrent"
+    @check_session
+    def POST(self, name):
+        vars = web.input(stop = None, start = None, redir = None)
+        if vars.stop:
+            proxy.pause_torrent(vars.stop)
+        elif vars.start:
+            proxy.resume_torrent(vars.start)
+
+        do_redirect()
+
+class torrent_add:
+    @deluge_page
+    def GET(self, name):
+        return render.torrent_add()
+
+    @check_session
+    def POST(self, name):
+
+        vars = web.input(url = None, torrent = {})
+
+        if vars.url and vars.torrent.filename:
+            error_page(_("Choose an url or a torrent, not both."))
+        if vars.url:
+            proxy.add_torrent_url(vars.url)
+            do_redirect()
+        elif vars.torrent.filename:
+            data = vars.torrent.file.read()
+            data_b64 = base64.b64encode(data)
+            #b64 because of strange bug-reports related to binary data
+            proxy.add_torrent_filecontent(vars.torrent.filename,data_b64)
+            do_redirect()
+        else:
+            error_page(_("no data."))
+
+class torrent_delete:
+    @deluge_page
+    def GET(self, torrent_id):
+        return render.torrent_delete(get_torrent_status(torrent_id))
+
+    @check_session
+    def POST(self, name):
+        torrent_id = name
+        vars = web.input(data_also = None, torrent_also = None)
+        data_also = bool(vars.data_also)
+        torrent_also = bool(vars.torrent_also)
+        proxy.remove_torrent(torrent_id, data_also, torrent_also)
+        do_redirect()
+
+class pause_all:
+    @check_session
+    def POST(self, name):
+        for torrent_id in proxy.get_torrent_state():
+            proxy.pause_torrent(torrent_id)
+        do_redirect()
+
+class resume_all:
+    @check_session
+    def POST(self, name):
+        for torrent_id in proxy.get_torrent_state():
+            proxy.resume_torrent(torrent_id)
+        do_redirect()
+
+class refresh:
+    @check_session
+    def POST(self, name):
+        auto_refresh = {'off':False, 'on':True}[name]
+        proxy.set_webui_config('auto_refresh', auto_refresh)
+        do_redirect()
+
+class refresh_set:
+    @deluge_page
+    def GET(self, name):
+        return render.refresh_form()
+
+    @check_session
+    def POST(self, name):
+        vars = web.input(refresh = 0)
+        refresh = int(vars.refresh)
+        if refresh > 0:
+            proxy.set_webui_config('refresh', refresh)
+            proxy.set_webui_config('auto_refresh', True)
+            do_redirect()
+        else:
+            error_page(_('refresh must be > 0'))
+
+#/pages
+
+if __name__ == "__main__":
+  web.run(urls, globals())
+
+
