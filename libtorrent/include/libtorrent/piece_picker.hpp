dbbace5f23bd51465d5e362ade3899286b5a18e5
Marcos Pinto <markybob@dipconsultants.com>
2007-10-24 00:30:21 +0000

    piece picker priority fix

--- libtorrent/include/libtorrent/piece_picker.hpp
+++ libtorrent/include/libtorrent/piece_picker.hpp
@@ -360,8 +360,8 @@ namespace libtorrent
 					case 2: return prio - 1;
 					case 3: return (std::max)(prio / 2, 1);
 					case 4: return (std::max)(prio / 2 - 1, 1);
-					case 5:
-					case 6: return (std::min)(prio / 2 - 1, 2);
+					case 5: return (std::max)(prio / 3, 1);
+					case 6: return (std::max)(prio / 3 - 1, 1);
 					case 7: return 1;
 				}
 				return prio;
