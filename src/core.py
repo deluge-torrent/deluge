b221e7ac829b7fa2740234d23461baba986cf9d9
Marcos Pinto <markybob@dipconsultants.com>
2007-11-24 04:58:57 +0000

    tweak full hd dialog

--- src/core.py
+++ src/core.py
@@ -544,11 +544,14 @@ class Manager:
                         # We have enough free space, so lets resume this torrent
                         self.resume(unique_ID)
                     else:
+                        nice_need = common.fsize(total_needed)
+                        nice_free = common.fsize(avail)
                         import gtk
                         import dialogs
                         gtk.gdk.threads_enter()
-                        result = dialogs.show_popup_warning(None, _("You're out \
-of HD space!  Oops!\nWe had to pause at least one torrent"))
+                        dialogs.show_popup_warning(None, _("There is not enough \
+free disk space to complete your download.") + "\n" + _("Space Needed:") + " " \
++ nice_need + "\n" + _("Available Space:") + " " + nice_free)
                         gtk.gdk.threads_leave()
                         print "Not enough free space to resume this torrent!"
                 else: #We're using compact allocation so lets just resume
@@ -856,7 +859,6 @@ of HD space!  Oops!\nWe had to pause at least one torrent"))
                                                         torrent.save_dir,
                                                         torrent.compact)
                 except DelugeError, e:
-                    print "Error probably bad torrent"
                     del self.state.torrents[torrent]
                     raise e
 
