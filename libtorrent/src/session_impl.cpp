431664fec42035ca2b4a27f3f8d9b4686a66cf85
Marcos Pinto <markybob@dipconsultants.com>
2007-12-24 22:27:26 +0000

    lt sync

--- libtorrent/src/session_impl.cpp
+++ libtorrent/src/session_impl.cpp
@@ -2198,6 +2198,11 @@ namespace detail
 #endif
 		m_checker_thread->join();
 
+#if defined(TORRENT_VERBOSE_LOGGING) || defined(TORRENT_LOGGING)
+		(*m_logger) << time_now_string() << " waiting for disk io thread\n";
+#endif
+		m_disk_thread.join();
+
 		TORRENT_ASSERT(m_torrents.empty());
 		TORRENT_ASSERT(m_connections.empty());
 #if defined(TORRENT_VERBOSE_LOGGING) || defined(TORRENT_LOGGING)
@@ -2347,6 +2352,7 @@ namespace detail
 			, bind(&session_impl::on_port_mapping
 				, this, _1, _2, _3));
 
+		m_upnp->discover_device();
 		m_upnp->set_mappings(m_listen_interface.port(), 
 #ifndef TORRENT_DISABLE_DHT
 			m_dht ? m_dht_settings.service_port : 
