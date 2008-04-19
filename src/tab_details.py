1b5bfaaad1147201ee17f6520970eb0c0cfe527c
Andrew Resch <andrewresch@gmail.com>
2008-04-19 16:07:02 +0000

    Fix possible incorrect display of uploaded total in details tab.

--- src/tab_details.py
+++ src/tab_details.py
@@ -154,13 +154,7 @@ class DetailsTabManager(object):
         self.total_downloaded.set_text('%s (%s)' % \
             (common.fsize(state["total_done"]),
              common.fsize(state["total_payload_download"])))
-        try:
-            self.total_uploaded.set_text('%s (%s)' % \
-            (common.fsize(self.manager.unique_IDs[unique_id].initial_uploaded_memory+\
-                          state["total_payload_upload"]),
-             common.fsize(state["total_payload_upload"])))
-        except AttributeError:
-            self.total_uploaded.set_text('%s (%s)' % \
+        self.total_uploaded.set_text('%s (%s)' % \
             (common.fsize(self.manager.unique_IDs[unique_id].uploaded_memory+\
                           state["total_payload_upload"]),
              common.fsize(state["total_payload_upload"])))            
