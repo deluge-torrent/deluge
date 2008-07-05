622ab78b943ccc245fb3eda894021e7d002fd427
Marcos Pinto <markybob@dipconsultants.com>
2008-07-05 00:57:04 +0000

    fix copyright symbol for some people

--- deluge/ui/gtkui/aboutdialog.py
+++ deluge/ui/gtkui/aboutdialog.py
@@ -55,8 +55,7 @@ class AboutDialog:
         version = deluge.common.get_version()
         if rev != "":
             version = version + "r" + rev
-
-        self.about.set_copyright("Copyright © 2007-2008 Andrew Resch")
+        self.about.set_copyright(u'Copyright \u00A9 2007-2008 Andrew Resch')
         self.about.set_comments("A peer-to-peer file sharing program utilizing the Bittorrent protocol.")
         self.about.set_version(version)
         self.about.set_authors(["Andrew Resch", "Marcos Pinto", 
