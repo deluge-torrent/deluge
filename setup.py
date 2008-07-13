e1fb908f6e7aaaea6014ce843713616220e82924
Andrew Resch <andrewresch@gmail.com>
2008-07-13 03:24:49 +0000

    Update version to 0.9.01 for RC

--- setup.py
+++ setup.py
@@ -324,7 +324,7 @@ setup(
                                 ]},
     packages = find_packages(exclude=["plugins"]),
     url = "http://deluge-torrent.org",
-    version = "0.6.0.0",
+    version = "0.9.01",
 )
     
 try:
