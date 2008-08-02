f853ac209b13a6fe7516aabc16b5b943946d2d92
Andrew Resch <andrewresch@gmail.com>
2008-08-02 03:25:34 +0000

    Handle shutting down more cleanly in the gtkui

--- deluge/ui/gtkui/gtkui.py
+++ deluge/ui/gtkui/gtkui.py
@@ -216,7 +216,7 @@ class GtkUI:
         else:           
             self.shutdown()
 
-    def shutdown(self, data=None):
+    def shutdown(self, *args, **kwargs):
         log.debug("gtkui shutting down..")
 
         # Make sure the config is saved.
@@ -229,7 +229,10 @@ class GtkUI:
                 client.shutdown()
             except:
                 pass
-
+        try:
+            gtk.main_quit()
+        except RuntimeError:
+            pass
             
     def _on_new_core(self, data):
         component.start()
