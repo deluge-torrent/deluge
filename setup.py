a2e0eae18a4674fa99b6c8d60518ac9e56f63c36
Andrew Resch <andrewresch@gmail.com>
2008-08-04 09:22:25 +0000

    Prep for next release

--- setup.py
+++ setup.py
@@ -323,7 +323,7 @@ setup(
                                 ]},
     packages = find_packages(exclude=["plugins"]),
     url = "http://deluge-torrent.org",
-    version = "0.9.05",
+    version = "0.9.06",
 )
 
 try:
