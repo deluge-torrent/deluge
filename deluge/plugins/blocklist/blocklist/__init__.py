6fa31d1dc0d0197b615bc36f42d7e5543745e971
Marcos Pinto <markybob@dipconsultants.com>
2008-08-08 05:59:07 +0000

    change copyright notices to gpl3

--- deluge/plugins/blocklist/blocklist/__init__.py
+++ deluge/plugins/blocklist/blocklist/__init__.py
@@ -9,7 +9,7 @@
 #
 # You may redistribute it and/or modify it under the terms of the
 # GNU General Public License, as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option)
+# Foundation; either version 3 of the License, or (at your option)
 # any later version.
 #
 # deluge is distributed in the hope that it will be useful,
