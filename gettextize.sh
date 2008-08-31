1e4f53b716374b39beeec4250580730bee4056be
Marcos Pinto <markybob@dipconsultants.com>
2008-08-31 23:37:02 +0000

    add gtk stock clearing script and update glade files

--- gettextize.sh
+++ gettextize.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
+for x in `find . -name '*.glade' |grep -v '.svn\|build'` ; do \
+    for y in gtk-apply gtk-bold gtk-cancel gtk-cdrom gtk-clear \
+        gtk-close gtk-color-picker gtk-convert gtk-copy gtk-cut \
+        gtk-delete gtk-dialog-error gtk-dialog-info gtk-dialog-question \
+        gtk-dialog-warning gtk-dnd gtk-dnd-multiple gtk-execute gtk-find \
+        gtk-find-and-replace gtk-floppy gtk-goto-bottom gtk-goto-first \
+        gtk-goto-last gtk-goto-top gtk-go-back gtk-go-down gtk-go-forward \
+        gtk-go-up gtk-help gtk-home gtk-index gtk-italic gtk-jump-to \
+        gtk-justify-center gtk-justify-fill gtk-justify-left gtk-missing-image \
+        gtk-new gtk-no gtk-ok gtk-open gtk-paste gtk-preferences gtk-print \
+        gtk-print-preview gtk-properties gtk-quit gtk-redo gtk-refresh \
+        gtk-remove gtk-revert-to-saved gtk-save gtk-save-as gtk-select-color \
+        gtk-select-font gtk-sort-descending gtk-spell-check gtk-stop \
+        gtk-strikethrough gtk-undelete gtk-underline gtk-undo gtk-yes \
+        gtk-zoom-100 gtk-zoom-fit gtk-zoom-in gtk-zoom-out; do \
+            sed -i "s/<property\ name\=\"label\"\ translatable\=\"yes\">$y<\/property>/<property\ name\=\"label\"\ translatable\=\"no\">$y<\/property>/g" $x; \
+        done;\
+    done
 xgettext -f deluge/i18n/POTFILES.in -o deluge/i18n/deluge.pot
