b9d1367564db6c4715bdb9fa6377221f2886a67d
Marcos Pinto <markybob@dipconsultants.com>
2008-09-13 03:10:52 +0000

    allow network mounts in right-click->move torrent

--- deluge/ui/gtkui/menubar.py
+++ deluge/ui/gtkui/menubar.py
@@ -337,6 +337,7 @@ class MenuBar(component.Component):
                 ) , component.get("MainWindow").window, \
                 gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER, buttons=(gtk.STOCK_CANCEL, \
                 gtk.RESPONSE_CANCEL, gtk.STOCK_OK, gtk.RESPONSE_OK))
+            chooser.set_local_only(False)
             if not common.windows_check():
                 chooser.set_icon(common.get_logo(18))
                 chooser.set_property("skip-taskbar-hint", True)
