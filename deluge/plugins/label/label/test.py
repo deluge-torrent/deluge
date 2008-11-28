256c5c71b8175767c2e3a92d6313662618e1540a
Andrew Resch <andrewresch@gmail.com>
2008-11-28 23:53:05 +0000

    Add copyright headers

--- deluge/plugins/label/label/test.py
+++ deluge/plugins/label/label/test.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Martijn Voncken 2008 <mvoncken@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, write to:
+#     The Free Software Foundation, Inc.,
+#     51 Franklin Street, Fifth Floor
+#     Boston, MA  02110-1301, USA.
+#
+
 from deluge.ui.client import sclient
 
 sclient.set_core_uri()
@@ -34,8 +56,3 @@ sclient.label_set_options("test",{"max_download_speed":888}, False)
 print sclient.get_torrent_status(id, ["max_download_speed"]) , "9 (888)"
 
 print sclient.get_torrent_status(id,['name', 'tracker_host', 'label'])
-
-
-
-
-
