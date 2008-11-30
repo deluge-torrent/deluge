00cf7a8f34f3857be7554d4773d3bc832d91bd51
Andrew Resch <andrewresch@gmail.com>
2008-11-30 19:27:51 +0000

    Fix typo

--- deluge/ui/webui/lib/webpy022/wsgiserver/__init__.py
+++ deluge/ui/webui/lib/webpy022/wsgiserver/__init__.py
@@ -9,7 +9,7 @@ Simplest example on how to use this module directly
 
     from cherrypy import wsgiserver
 
-    def my_crazy_a pp(environ, start_response):
+    def my_crazy_app(environ, start_response):
         status = '200 OK'
         response_headers = [('Content-type','text/plain')]
         start_response(status, response_headers)
