650d4d06a2e5ef35befe467979d4f2ec60390af0
Martijn Voncken <mvoncken@gmail.com>
2008-12-06 21:52:56 +0000

    move ajax-ui to oxygen icon set

--- deluge/ui/webui/scripts/copy_icons.py
+++ deluge/ui/webui/scripts/copy_icons.py
@@ -22,18 +22,24 @@
 
 """
 copy  icons from kde icon set.
-Edit ICON_SET and TARGET before running this file.
+Edit ICON_SET and TARGET_DIR before running this file.
 #perl -pi -w -e 's/tango/16/g;' *.html
 """
 import os
 from os import path
 from shutil import copyfile
 
-ICON_SET  = "/home/martijn/prj/oxygen/oxygen/16x16"
-TARGET_DIR = "/home/martijn/src/deluge/deluge/ui/webui/static/images/16"
+ICON_SET  = "/home/martijn/prj/oxygen/oxygen"
+TARGET_DIR = "/home/martijn/src/deluge/deluge/ui/webui"
 
+def copy_icons(source_dir , target_dir , mapping):
+    for target, source in mapping.iteritems():
+        source = path.join(source_dir, source) +  ".png"
+        target = path.join(target_dir, target) +  ".png"
+        print "%s -> %s" % (source, target)
+        copyfile(source, target)
 
-mapping = {
+map_static16 = {
     "down":"actions/1downarrow",
     "up":"actions/1downarrow",
     "connections":"apps/preferences_system_network_sharing",
@@ -61,9 +67,30 @@ mapping = {
     "select-all":"actions/edit_select_all"
 }
 
-for target, source in mapping.iteritems():
-    source = path.join(ICON_SET, source) +  ".png"
-    target = path.join(TARGET_DIR, target) +  ".png"
-    print "%s->%s" % (source, target)
-    copyfile(source, target)
+map_ajax16 = {
+    "gtk-edit":"actions/object_edit",
+    "gtk-yes":"actions/flag_green",
+    "network-idle":"apps/preferences_system_network_sharing",
+    "view-refresh":"actions/view_refresh",
+    "view-sort-ascending":"actions/view_sort_ascending",
+    "view-sort-descending":"actions/view_sort_descending"
+}
+
+map_ajax32 = {
+    "add":"actions/add",
+    "connections":"apps/preferences_system_network_sharing",
+    "down":"actions/1downarrow",
+    "up":"actions/1uparrow",
+    "new":"actions/document_new",
+    "options":"apps/preferences_system",
+    "pause":"actions/media_playback_pause",
+    "remove":"actions/fileclose",
+    "resume":"actions/media_playback_start"
+}
+
+
+copy_icons( path.join(ICON_SET, "16x16"), path.join(TARGET_DIR, "static/images/16"), map_static16)
+copy_icons( path.join(ICON_SET, "16x16"), path.join(TARGET_DIR, "templates/ajax/static/icons/16"), map_ajax16)
+copy_icons( path.join(ICON_SET, "32x32"), path.join(TARGET_DIR, "templates/ajax/static/icons/32"), map_ajax32)
+
 
