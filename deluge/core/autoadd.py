d4445adf20438a8a43f770b0574965cc591e365e
Andrew Resch <andrewresch@gmail.com>
2008-12-16 04:32:03 +0000

    Fix importing system libtorrent

--- deluge/core/autoadd.py
+++ deluge/core/autoadd.py
@@ -29,7 +29,7 @@ try:
     import deluge.libtorrent as lt
 except ImportError:
     import libtorrent as lt
-    if not (libtorrent.version_major == 0 and libtorrent.version_minor == 14):
+    if not (lt.version_major == 0 and lt.version_minor == 14):
         raise ImportError("This version of Deluge requires libtorrent 0.14!")
 
 import deluge.component as component
