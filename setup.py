34824793f261e00f0348925b6cd6cad42258b617
Andrew Resch <andrewresch@gmail.com>
2008-12-17 00:11:00 +0000

    Don't include LICENSE or readme.txt in package data

--- setup.py
+++ setup.py
@@ -380,7 +380,7 @@ setup(
                                 "ui/webui/static/images/*.png",
                                 "ui/webui/static/images/*.jpg",
                                 "ui/webui/static/images/*.gif",
-                                "ui/webui/static/images/16/*",
+                                "ui/webui/static/images/16/*.png",
                                 "ui/webui/templates/deluge/*",
                                 "ui/webui/templates/classic/*",
                                 "ui/webui/templates/white/*"
