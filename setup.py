c3697f95e90b3ef35bf874b8134673f94b8c7bce
Andrew Resch <andrewresch@gmail.com>
2008-12-18 02:52:24 +0000

    Branch 1.1.0, trunk is now 1.2.0-dev

--- setup.py
+++ setup.py
@@ -401,5 +401,5 @@ setup(
                                 ]},
     packages = find_packages(exclude=["plugins"]),
     url = "http://deluge-torrent.org",
-    version = "1.1.0_dev",
+    version = "1.2.0_dev",
 )
