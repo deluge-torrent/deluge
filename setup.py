d25b41f1de26466f8cc1819a124cc56a541f9856
Marcos Pinto <markybob@dipconsultants.com>
2008-12-29 06:50:51 +0000

    version prep for release

--- setup.py
+++ setup.py
@@ -402,5 +402,5 @@ setup(
                                 ]},
     packages = find_packages(exclude=["plugins"]),
     url = "http://deluge-torrent.org",
-    version = "1.1.0_RC1",
+    version = "1.1.0_RC2",
 )
