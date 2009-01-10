2f3edc0352c97bba70946adccfa11ed5ca26df64
Andrew Resch <andrewresch@gmail.com>
2009-01-10 00:05:31 +0000

    Merge copyright info from trunk

--- deluge/ui/console/colors.py
+++ deluge/ui/console/colors.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
+#
+# colors.py
+#
+# Copyright (C) 2008-2009 Ido Abramovich <ido.deluge@gmail.com>
+#
+# Deluge is free software.
+#
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# deluge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with deluge.    If not, write to:
+# 	The Free Software Foundation, Inc.,
+# 	51 Franklin Street, Fifth Floor
+# 	Boston, MA    02110-1301, USA.
+#
 import re, sys
 
 def color(string, fg=None, attrs=[], bg=None, keep_open=False, input=False):
