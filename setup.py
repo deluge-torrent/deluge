2de48097c035a0fbee6848626c2641b6d88dcfac
Andrew Resch <andrewresch@gmail.com>
2009-01-25 00:33:49 +0000

    Prep for release

--- setup.py
+++ setup.py
@@ -413,5 +413,5 @@ setup(
                                 ]},
     packages = find_packages(exclude=["plugins"]),
     url = "http://deluge-torrent.org",
-    version = "1.1.0",
+    version = "1.1.1",
 )
