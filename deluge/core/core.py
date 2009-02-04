e861e660a5b78e7340b0bc1f413e77a906d43b26
Andrew Resch <andrewresch@gmail.com>
2009-02-04 14:55:51 +0000

    Fix queue order changing

--- deluge/core/core.py
+++ deluge/core/core.py
@@ -681,6 +681,7 @@ class Core(component.Component):
     def queue_up(self, torrent_ids):
         log.debug("Attempting to queue %s to up", torrent_ids)
         #torrent_ids must be sorted before moving.
+        torrent_ids = list(torrent_ids)
         torrent_ids.sort(key = lambda id: self.torrentmanager.torrents[id].get_queue_position())
         for torrent_id in torrent_ids:
             try:
@@ -694,6 +695,7 @@ class Core(component.Component):
     def queue_down(self, torrent_ids):
         log.debug("Attempting to queue %s to down", torrent_ids)
         #torrent_ids must be sorted before moving.
+        torrent_ids = list(torrent_ids)
         torrent_ids.sort(key = lambda id: -self.torrentmanager.torrents[id].get_queue_position())
         for torrent_id in torrent_ids:
             try:
