f14e5b08f4001a0581465035b02ec47e4b3fa457
Andrew Resch <andrewresch@gmail.com>
2009-02-05 04:46:38 +0000

    Update license on metafile.py to GPL3

--- deluge/metafile.py
+++ deluge/metafile.py
@@ -1,14 +1,20 @@
-# The contents of this file are subject to the BitTorrent Open Source License
-# Version 1.1 (the License).  You may not copy or use this file, in either
-# source code or executable form, except in compliance with the License.  You
-# may obtain a copy of the License at http://www.bittorrent.com/license/.
+# Taken from http://download.bittorrent.com/dl/BitTorrent-5.3-GPL.tar.gz
 #
-# Software distributed under the License is distributed on an AS IS basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied.  See the License
-# for the specific language governing rights and limitations under the
-# License.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Written by Bram Cohen
+# Modifications for use in Deluge by Andrew Resch 2008
 
 import os
 import os.path
