345b28935fdc497add4ec2fcf626caf1941e3a10
Damien Churchill <damoc@gmail.com>
2009-02-12 00:14:59 +0000

    make enter check the password

--- deluge/ui/web/js/deluge-login.js
+++ deluge/ui/web/js/deluge-login.js
@@ -1,18 +1,4 @@
 Deluge.Login = {
-    Form: new Ext.form.FormPanel({
-        defaultType: 'textfield',
-        id: 'loginForm',
-        baseCls: 'x-plain',
-        labelWidth: 55,
-        items: [{
-            fieldLabel: _('Password'),
-            id: 'password',
-            name: 'password',
-            inputType: 'password',
-            anchor: '100%'
-        }]
-    }),
-    
     onLogin: function() {
         var passwordField = Deluge.Login.Form.items.get('password');
         Deluge.Client.web.login(passwordField.getValue(), {
@@ -30,8 +16,32 @@ Deluge.Login = {
                 }
             }
         });
+    },
+    
+    onKey: function(field, e) {
+        if (e.getKey() == 13) Deluge.Login.onLogin();
     }
 }
+
+Deluge.Login.Form = new Ext.form.FormPanel({
+    defaultType: 'textfield',
+    id: 'loginForm',
+    baseCls: 'x-plain',
+    labelWidth: 55,
+    items: [{
+        fieldLabel: _('Password'),
+        id: 'password',
+        name: 'password',
+        inputType: 'password',
+        anchor: '100%',
+        listeners: {
+            'specialkey': {
+                fn: Deluge.Login.onKey,
+                scope: Deluge.Login
+            }
+        }
+    }]
+});
 
 Deluge.Login.Window = new Ext.Window({
     layout: 'fit',
