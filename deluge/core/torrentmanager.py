1375d4f545f85971263697442714881fa4bf3090
Andrew Resch <andrewresch@gmail.com>
2009-02-28 21:59:49 +0000

    Fix torrentmanagers update interval to 5 seconds

--- deluge/core/torrentmanager.py
+++ deluge/core/torrentmanager.py
@@ -110,7 +110,7 @@ class TorrentManager(component.Component):
     session for use on restart."""
 
     def __init__(self, session, alerts):
-        component.Component.__init__(self, "TorrentManager", interval=5000, depend=["CorePluginManager"])
+        component.Component.__init__(self, "TorrentManager", interval=5, depend=["CorePluginManager"])
         log.debug("TorrentManager init..")
         # Set the libtorrent session
         self.session = session
