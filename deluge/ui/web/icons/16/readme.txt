d9d5d2b308c32b2e1a07831bc8b4f6f83af65872
Damien Churchill <damoc@gmail.com>
2009-03-16 17:07:13 +0000

    remove all remaining oxygen icons add the additional required icons from fugue add the torrents context menu

--- deluge/ui/web/icons/16/readme.txt
+++ deluge/ui/web/icons/16/readme.txt
@@ -1,5 +1,2 @@
-images from the kde-oxygen set.
-See webui/scripts/copy_icons.py for mapping.
-See LICENSE for a list of icons not taken from oxygen.
-
-
+images from the fruge icon set.
+See LICENSE for a list of icons not taken from fruge.
--- deluge/ui/web/js/deluge-menus.js
+++ deluge/ui/web/js/deluge-menus.js
@@ -21,7 +21,141 @@ Copyright:
 		Boston, MA  02110-1301, USA.
 */
 
-Deluge.Menus = {}
+Deluge.Menus = {
+	onTorrentAction: function(e) {
+		
+	}
+}
+
+Deluge.Menus.Torrent = new Ext.menu.Menu({
+	id: 'torrentMenu',
+	items: [{
+		id: 'pause',
+		text: _('Pause'),
+		handler: Deluge.Menus.onTorrentAction,
+		scope: Deluge.Menus,
+		icon: '/icons/16/pause.png'
+	}, {
+		id: 'resume',
+		text: _('Resume'),
+		icon: '/icons/16/start.png'
+	}, '-', {
+		id: 'options',
+		text: _('Options'),
+		icon: '/icons/16/preferences.png',
+		menu: new Ext.menu.Menu({
+			items: [{
+				text: _('D/L Speed Limit'),
+				iconCls: 'x-deluge-downloading',
+				menu: new Ext.menu.Menu({
+					items: [{
+						text: _('5 KiB/s'),
+					}, {
+						text: _('10 KiB/s'),
+					}, {
+						text: _('30 KiB/s'),
+					}, {
+						text: _('80 KiB/s'),
+					}, {
+						text: _('300 KiB/s'),
+					},{
+						text: _('Unlimited')
+					}]
+				})
+			}, {
+				text: _('U/L Speed Limit'),
+				iconCls: 'x-deluge-seeding',
+				menu: new Ext.menu.Menu({
+					items: [{
+						text: _('5 KiB/s'),
+					}, {
+						text: _('10 KiB/s'),
+					}, {
+						text: _('30 KiB/s'),
+					}, {
+						text: _('80 KiB/s'),
+					}, {
+						text: _('300 KiB/s'),
+					},{
+						text: _('Unlimited')
+					}]
+				})
+			}, {
+				text: _('Connection Limit'),
+				icon: '/icons/16/connections.png',
+				menu: new Ext.menu.Menu({
+					items: [{
+						text: _('50'),
+					}, {
+						text: _('100'),
+					}, {
+						text: _('200'),
+					}, {
+						text: _('300'),
+					}, {
+						text: _('500'),
+					},{
+						text: _('Unlimited')
+					}]
+				})
+			}, {
+				text: _('Upload Slot Limit'),
+				icon: '/icons/16/upload_slots.png',
+				menu: new Ext.menu.Menu({
+					items: [{
+						text: _('0'),
+					}, {
+						text: _('1'),
+					}, {
+						text: _('2'),
+					}, {
+						text: _('3'),
+					}, {
+						text: _('5'),
+					},{
+						text: _('Unlimited')
+					}]
+				})
+			}, {
+				text: _('Auto Managed'),
+				checked: false
+			}]
+		})
+	}, '-', {
+		text: _('Queue'),
+		icon: '/icons/16/queue.png',
+		menu: new Ext.menu.Menu({
+			items: [{
+				text: _('Top'),
+				icon: '/icons/16/top.png'
+			},{
+				text: _('Up'),
+				icon: '/icons/16/up.png'
+			},{
+				text: _('Down'),
+				icon: '/icons/16/down.png'
+			},{
+				text: _('Bottom'),
+				icon: '/icons/16/bottom.png'
+			}]
+		})
+	}, '-', {
+		text: _('Update Tracker'),
+		icon: '/icons/16/update.png'
+	}, {
+		text: _('Edit Trackers'),
+		icon: '/icons/16/edit_trackers.png'
+	}, '-', {
+		text: _('Remove Torrent'),
+		icon: '/icons/16/remove.png'
+	}, '-', {
+		text: _('Force Recheck'),
+		icon: '/icons/16/recheck.png'
+	}, {
+		text: _('Move Storage'),
+		icon: '/icons/16/move.png'
+	}]
+});
 
 Deluge.Menus.Connections = new Ext.menu.Menu({
 	id: 'connectionsMenu',
