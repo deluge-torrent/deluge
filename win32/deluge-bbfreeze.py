31780d263321332ca17105200b683d336f8da768
Jesper Lund <mail@jesperlund.com>
2009-04-17 00:03:53 +0000

    Added bbfreeze and NSIS scripts for Windows version

---
+++ win32/deluge-bbfreeze.py
@@ -0,0 +1,12 @@
+build_version = "1.1.6"
+python_path = "C:\\Python25\\"
+
+import shutil
+shutil.copy(python_path + "Scripts\deluge-script.py", python_path + "Scripts\deluge.py")
+shutil.copy(python_path + "Scripts\deluged-script.py", python_path + "Scripts\deluged.py")
+
+from bbfreeze import Freezer
+f = Freezer("..\\build-win32\\deluge-bbfreeze-" + build_version, includes=("gzip", "zipfile", "re", "socket", "struct", "cairo", "pangocairo", "atk", "pango"))
+f.addScript(python_path + "Scripts\deluge.py", gui_only=False)
+f.addScript(python_path + "Scripts\deluged.py", gui_only=False)
+f()    # starts the freezing process
