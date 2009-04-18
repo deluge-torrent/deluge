286619f4e3617c3f1d42edd87b30559748415bc1
Damien Churchill <damoc@gmail.com>
2009-04-18 12:50:34 +0000

    add FILE_PRIORITY_CSS and run FILE_PRIORITY through gettext

--- deluge/ui/web/js/deluge.js
+++ deluge/ui/web/js/deluge.js
@@ -121,14 +121,21 @@ Deluge.Formatters = {
 }
 
 FILE_PRIORITY = {
-    0: 'Do Not Download',
-    1: 'Normal Priority',
-    2: 'High Priority',
-    5: 'Highest Priority',
-    'Do Not Download': 0,
-    'Normal Priority': 1,
-    'High Priority': 2,
-    'Highest Priority': 5
+    0: _('Do Not Download'),
+    1: _('Normal Priority'),
+    2: _('High Priority'),
+    5: _('Highest Priority'),
+    _('Do Not Download'): 0,
+    _('Normal Priority'): 1,
+    _('High Priority'): 2,
+    _('Highest Priority'): 5
+}
+
+FILE_PRIORITY_CSS = {
+	0: 'x-no-download',
+	1: 'x-normal-download',
+	2: 'x-high-download',
+	5: 'x-highest-download'
 }
 
 var fsize = Deluge.Formatters.size;
