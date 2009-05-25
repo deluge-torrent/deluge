b883ffc474aead68f6086bc924e21675c6bc1993
Andrew Resch <andrewresch@gmail.com>
2009-05-25 22:55:42 +0000

    Fix exception when trackers are disabled from the sidebar

--- deluge/core/filtermanager.py
+++ deluge/core/filtermanager.py
@@ -196,7 +196,8 @@ class FilterManager(component.Component):
                 value = status[field]
                 items[field][value] = items[field].get(value, 0) + 1
 
-        items["tracker_host"]["Error"] = len(tracker_error_filter(torrent_ids, ("Error",)))
+        if "tracker_host" in items:
+            items["tracker_host"]["Error"] = len(tracker_error_filter(torrent_ids, ("Error",)))
 
         if "state" in tree_keys and not show_zero_hits:
             self._hide_state_items(items["state"])
