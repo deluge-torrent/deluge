c154301dbde813ba3c86014181ae4122f97a6181
Andrew Resch <andrewresch@gmail.com>
2009-08-06 20:16:23 +0000

    Add a force recheck command to the console

---
+++ deluge/ui/console/commands/recheck.py
@@ -0,0 +1,62 @@
+#
+# recheck.py
+#
+# Copyright (C) 2009 Andrew Resch <andrewresch@gmail.com>
+#
+# Deluge is free software.
+#
+# You may redistribute it and/or modify it under the terms of the
+# GNU General Public License, as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# deluge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with deluge.    If not, write to:
+# 	The Free Software Foundation, Inc.,
+# 	51 Franklin Street, Fifth Floor
+# 	Boston, MA  02110-1301, USA.
+#
+#    In addition, as a special exception, the copyright holders give
+#    permission to link the code of portions of this program with the OpenSSL
+#    library.
+#    You must obey the GNU General Public License in all respects for all of
+#    the code used other than OpenSSL. If you modify file(s) with this
+#    exception, you may extend this exception to your version of the file(s),
+#    but you are not obligated to do so. If you do not wish to do so, delete
+#    this exception statement from your version. If you delete this exception
+#    statement from all source files in the program, then also delete it here.
+#
+#
+from deluge.ui.console.main import BaseCommand
+from deluge.ui.client import client
+import deluge.ui.console.colors as colors
+import deluge.component as component
+
+class Command(BaseCommand):
+    """Forces a recheck of the torrent data"""
+    usage = "Usage: recheck [ * | <torrent-id> [<torrent-id> ...] ]"
+    def handle(self, *args, **options):
+        self.console = component.get("ConsoleUI")
+
+        if len(args) == 0:
+            self.console.write(self.usage)
+            return
+        if len(args) > 0 and args[0].lower() == '*':
+            client.core.force_recheck(self.console.match_torrent(""))
+            return
+
+        torrent_ids = []
+        for arg in args:
+            torrent_ids.extend(self.console.match_torrent(arg))
+
+        if torrent_ids:
+            return client.core.force_recheck(torrent_ids)
+
+    def complete(self, line):
+        # We use the ConsoleUI torrent tab complete method
+        return component.get("ConsoleUI").tab_complete_torrent(line)
