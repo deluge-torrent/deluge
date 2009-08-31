5d85af9adf8aaf1bedeca6ba7fabc17880049b79
Andrew Resch <andrewresch@gmail.com>
2009-08-31 22:16:39 +0000

    Add SSL exception to license in the about dialog

--- deluge/ui/gtkui/aboutdialog.py
+++ deluge/ui/gtkui/aboutdialog.py
@@ -70,7 +70,15 @@ later version. This program is distributed in the hope that it will be useful, b
 WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS \
 FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You \
 should have received a copy of the GNU General Public License along with this program; \
-if not, see <http://www.gnu.org/licenses>."))
+if not, see <http://www.gnu.org/licenses>. \
+In addition, as a special exception, the copyright holders give \
+permission to link the code of portions of this program with the OpenSSL library. \
+You must obey the GNU General Public License in all respects for all of \
+the code used other than OpenSSL. If you modify file(s) with this \
+exception, you may extend this exception to your version of the file(s), \
+but you are not obligated to do so. If you do not wish to do so, delete \
+this exception statement from your version. If you delete this exception \
+statement from all source files in the program, then also delete it here."))
         self.about.set_website("http://deluge-torrent.org")
         self.about.set_website_label("http://deluge-torrent.org")
 
