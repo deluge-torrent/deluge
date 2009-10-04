ebd0290e44e8bbf60ef8e2fd5229832ac06436d2
Andrew Resch <andrewresch@gmail.com>
2009-10-04 23:48:18 +0000

    Add script to regenerate the POTFILES.in file Update the POTFILES.in file

---
+++ create_potfiles_in.py
@@ -0,0 +1,23 @@
+import os
+
+# Paths to exclude
+EXCLUSIONS = [
+    "deluge/scripts"
+]
+
+POTFILE_IN = "deluge/i18n/POTFILES.in"
+
+print "Creating " + POTFILE_IN + " .."
+to_translate = []
+for (dirpath, dirnames, filenames) in os.walk("deluge"):
+    for filename in filenames:
+        if os.path.splitext(filename)[1] in (".py", ".glade") and dirpath not in EXCLUSIONS:
+            to_translate.append(os.path.join(dirpath, filename))
+
+f = open(POTFILE_IN, "wb")
+for line in to_translate:
+    f.write(line + "\n")
+
+f.close()
+
+print "Done"
