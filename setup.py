644f0ecb38e63530d204e5fc113cce46bf61682a
Andrew Resch <andrewresch@gmail.com>
2009-10-08 02:51:49 +0000

    Modify setup.py to allow building without libtorrent/

--- setup.py
+++ setup.py
@@ -207,7 +207,7 @@ except ImportError:
 else:
     build_libtorrent = False
 
-if build_libtorrent:
+if build_libtorrent and os.path.exists("libtorrent"):
     # There isn't a system libtorrent library, so let's build the one included with deluge
     libtorrent = Extension(
         'libtorrent',
