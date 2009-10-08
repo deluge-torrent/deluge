8f50687034758a0273fa1a87cc3dabc766ce9023
Andrew Resch <andrewresch@gmail.com>
2009-10-08 02:42:00 +0000

    Make sure libtorrent.pyd is included in bbfreeze

--- win32/deluge-bbfreeze.py
+++ win32/deluge-bbfreeze.py
@@ -6,7 +6,7 @@ shutil.copy(python_path + "Scripts\deluge-script.py", python_path + "Scripts\del
 shutil.copy(python_path + "Scripts\deluged-script.py", python_path + "Scripts\deluged.py")
 
 from bbfreeze import Freezer
-f = Freezer("..\\build-win32\\deluge-bbfreeze-" + build_version, includes=("gzip", "zipfile", "re", "socket", "struct", "cairo", "pangocairo", "atk", "pango"))
+f = Freezer("..\\build-win32\\deluge-bbfreeze-" + build_version, includes=("libtorrent", "gzip", "zipfile", "re", "socket", "struct", "cairo", "pangocairo", "atk", "pango"))
 f.addScript(python_path + "Scripts\deluge.py", gui_only=False)
 f.addScript(python_path + "Scripts\deluged.py", gui_only=False)
 f()    # starts the freezing process
