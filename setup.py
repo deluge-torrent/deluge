4f28ab9a39f83a20b114b8cac608aa2e7e161918
Damien Churchill <damoc@gmail.com>
2010-01-12 17:21:00 +0000

    bump version number so its now 1.2.1-dev

--- setup.py
+++ setup.py
@@ -397,7 +397,7 @@ _data_files = [
 # Main setup
 setup(
     name = "deluge",
-    version = "1.2.0",
+    version = "1.2.1",
     fullname = "Deluge Bittorrent Client",
     description = "Bittorrent Client",
     author = "Andrew Resch, Damien Churchill",
