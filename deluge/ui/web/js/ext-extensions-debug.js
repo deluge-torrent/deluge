031f75a2bb56d376926801f3fbe498d195c64a2e
Damien Churchill <damoc@gmail.com>
2010-01-23 15:34:50 +0000

    disable the override to formlayout for the time being

--- deluge/ui/web/js/ext-extensions-debug.js
+++ deluge/ui/web/js/ext-extensions-debug.js
@@ -1027,36 +1027,36 @@ Ext.reg('uxspinnergroup', Ext.ux.form.SpinnerGroup);
 
 // Taken from http://extjs.com/forum/showthread.php?t=75273
 // remove spaces for hidden elements and make show(), hide(), enable() and disable() act on the label. don't use hideLabel with this
-Ext.override(Ext.layout.FormLayout, {
-	renderItem: function(c, position, target) {
-		if (c && !c.rendered && c.isFormField && c.inputType != 'hidden') {
-			var args = [
-				c.id, c.fieldLabel,
-				c.labelStyle||this.labelStyle||'',
-				this.elementStyle||'',
-				typeof c.labelSeparator == 'undefined' ? this.labelSeparator : c.labelSeparator,
-				(c.itemCls||this.container.itemCls||'') + (c.hideLabel ? ' x-hide-label' : ''),
-				c.clearCls || 'x-form-clear-left'
-			];
-			if(typeof position == 'number') {
-				position = target.dom.childNodes[position] || null;
-			}
-			if (position) {
-				c.formItem = this.fieldTpl.insertBefore(position, args, true);
-			}
-			else {
-				c.formItem = this.fieldTpl.append(target, args, true);
-			}
-			c.actionMode = 'formItem';
-			c.render('x-form-el-'+c.id);
-			c.container = c.formItem;
-			c.actionMode = 'container';
-		}
-		else {
-			Ext.layout.FormLayout.superclass.renderItem.apply(this, arguments);
-		}
-	}
-});
+//Ext.override(Ext.layout.FormLayout, {
+//	renderItem: function(c, position, target) {
+//		if (c && !c.rendered && c.isFormField && c.inputType != 'hidden') {
+//			var args = [
+//				c.id, c.fieldLabel,
+//				c.labelStyle||this.labelStyle||'',
+//				this.elementStyle||'',
+//				typeof c.labelSeparator == 'undefined' ? this.labelSeparator : c.labelSeparator,
+//				(c.itemCls||this.container.itemCls||'') + (c.hideLabel ? ' x-hide-label' : ''),
+//				c.clearCls || 'x-form-clear-left'
+//			];
+//			if(typeof position == 'number') {
+//				position = target.dom.childNodes[position] || null;
+//			}
+//			if (position) {
+//				c.formItem = this.fieldTpl.insertBefore(position, args, true);
+//			}
+//			else {
+//				c.formItem = this.fieldTpl.append(target, args, true);
+//			}
+//			c.actionMode = 'formItem';
+//			c.render('x-form-el-'+c.id);
+//			c.container = c.formItem;
+//			c.actionMode = 'container';
+//		}
+//		else {
+//			Ext.layout.FormLayout.superclass.renderItem.apply(this, arguments);
+//		}
+//	}
+//});
 Ext.override(Ext.form.TriggerField, {
 	actionMode: 'wrap',
 	onShow: Ext.form.TriggerField.superclass.onShow,
