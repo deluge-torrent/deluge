744f11e19b53eb16f8f7ff1bed3d970420358f27
Damien Churchill <damoc@gmail.com>
2010-01-26 13:11:29 +0000

    rename the TreeGridCustomColumn to TreeGridRenderColumn and add the column object to the template so its accessible from the renderer

--- deluge/ui/web/js/ext-extensions/TreeGridCustomColumn.js
+++
@@ -1,9 +0,0 @@
-Ext.tree.CustomColumn = Ext.extend(Ext.tree.Column, {
-	
-	constructor: function(c) {
-		c.tpl = c.tpl || new Ext.XTemplate('{' + c.dataIndex + ':this.format}');
-		c.tpl.format = c.renderer;
-		Ext.tree.CustomColumn.superclass.constructor.call(this, c);
-	}
-});
-Ext.reg('tgcustomcolumn', Ext.tree.CustomColumn);
---
+++ deluge/ui/web/js/ext-extensions/TreeGridRenderColumn.js
@@ -0,0 +1,10 @@
+Ext.tree.RenderColumn = Ext.extend(Ext.tree.Column, {
+	
+	constructor: function(c) {
+		c.tpl = c.tpl || new Ext.XTemplate('{' + c.dataIndex + ':this.format}');
+		c.tpl.format = c.renderer;
+		c.tpl.col = this;
+		Ext.tree.RenderColumn.superclass.constructor.call(this, c);
+	}
+});
+Ext.reg('tgrendercolumn', Ext.tree.RenderColumn);
