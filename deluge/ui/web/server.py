976824ad99aa4f5912c26bf01b1491af7eab56f2
Damien Churchill <damoc@gmail.com>
2010-01-27 20:23:15 +0000

    revert to the default theme if the selected themes stylesheet is missing

--- deluge/ui/web/server.py
+++ deluge/ui/web/server.py
@@ -400,7 +400,10 @@ class TopLevel(resource.Resource):
         self.putChild("tracker", Tracker())
 
         theme = component.get("DelugeWeb").config["theme"]
+        if not os.path.isfile(rpath("css", "xtheme-%s.css" % theme)):
+            theme = CONFIG_DEFAULTS.get("theme")
         self.__stylesheets.insert(1, "/css/xtheme-%s.css" % theme)
+
 
     @property
     def scripts(self):
