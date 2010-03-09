a961947720ffc4a8324a53e374e412edf391ef3d
Damien Churchill <damoxc@gmail.com>
2010-03-09 22:14:33 +0000

    add a toolbar to the file browser

--- deluge/ui/web/js/deluge-all/Deluge.FileBrowser.js
+++ deluge/ui/web/js/deluge-all/Deluge.FileBrowser.js
@@ -2,6 +2,16 @@ Ext.namespace('Deluge');
 Deluge.FileBrowser = Ext.extend(Ext.Window, {
 
 	title: 'Filebrowser',
+
+	width:  500,
+	height: 400,
+
+	tbar: {
+		items: [{
+			cls: 'x-btn-icon',
+			iconCls: 'icon-back'
+		}]
+	},
 	
 	initComponent: function() {
 		Deluge.FileBrowser.superclass.initComponent.call(this);
