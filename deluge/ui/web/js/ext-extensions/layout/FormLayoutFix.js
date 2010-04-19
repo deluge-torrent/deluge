771123945276b5787006322359cbfc58cdd69d7c
Damien Churchill <damoxc@gmail.com>
2010-04-19 22:29:11 +0100

    add the updated override to FormLayout to allow for the field labels to be hidden/shown with field.show()/field.hide()

---
+++ deluge/ui/web/js/ext-extensions/layout/FormLayoutFix.js
@@ -0,0 +1,56 @@
+/*!
+ * Ext.ux.layout.FormLayoutFix.js
+ * 
+ * Copyright (c) Damien Churchill 2009-2010 <damoxc@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, write to:
+ *     The Free Software Foundation, Inc.,
+ *     51 Franklin Street, Fifth Floor
+ *     Boston, MA  02110-1301, USA.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the OpenSSL
+ * library.
+ * You must obey the GNU General Public License in all respects for all of
+ * the code used other than OpenSSL. If you modify file(s) with this
+ * exception, you may extend this exception to your version of the file(s),
+ * but you are not obligated to do so. If you do not wish to do so, delete
+ * this exception statement from your version. If you delete this exception
+ * statement from all source files in the program, then also delete it here.
+ */
+
+// Taken from http://extjs.com/forum/showthread.php?t=75273
+// remove spaces for hidden elements and make show(), hide(), enable() and disable() act on
+// the label. don't use hideLabel with this.
+Ext.override(Ext.layout.FormLayout, {
+	renderItem : function(c, position, target){
+		if(c && !c.rendered && (c.isFormField || c.fieldLabel) && c.inputType != 'hidden'){
+			var args = this.getTemplateArgs(c);
+			if(typeof position == 'number'){
+				position = target.dom.childNodes[position] || null;
+			}
+			if(position){
+				c.formItem = this.fieldTpl.insertBefore(position, args, true);
+			}else{
+				c.formItem = this.fieldTpl.append(target, args, true);
+			}
+			c.actionMode = 'formItem';
+			c.render('x-form-el-'+c.id);
+			c.container = c.formItem;
+			c.actionMode = 'container';
+		}else {
+			Ext.layout.FormLayout.superclass.renderItem.apply(this, arguments);
+		}
+	}
+});
