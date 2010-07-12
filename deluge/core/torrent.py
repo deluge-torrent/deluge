72ec926c1afdb312ad51dfbbac117c1775abfdb6
Andrew Resch <andrewresch@gmail.com>
2010-07-12 14:45:13 -0700

    Do not attempt to move a torrents storage if the destination path does not exist

--- deluge/core/torrent.py
+++ deluge/core/torrent.py
@@ -794,6 +794,9 @@ class Torrent(object):
 
     def move_storage(self, dest):
         """Move a torrent's storage location"""
+        if not os.path.exists(dest):
+            log.error("Could not move storage for torrent %s since %s does not exist!", self.torrent_id, dest)
+            return False
         try:
             self.handle.move_storage(dest.encode("utf8"))
         except:
