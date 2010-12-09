f17634ea638853bc7405f8c104edcb03fa0ac413
Pedro Algarvio <ufs@ufsoft.org>
2010-12-09 22:18:59 +0000

    Drop pkutil and simply use pkg_resources.

--- deluge/__init__.py
+++ deluge/__init__.py
@@ -1,8 +1,4 @@
 """Deluge"""
 # this is a namespace package
-try:
-    import pkg_resources
-    pkg_resources.declare_namespace(__name__)
-except ImportError:
-    import pkgutil
-    __path__ = pkgutil.extend_path(__path__, __name__)
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
