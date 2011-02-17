e1a3a431f07db9c6ca3db41b6b2cb6d54812600a
Nick <nick@afternight.org>
2011-02-17 16:26:58 +0100

    support command line options again

--- deluge/ui/console/main.py
+++ deluge/ui/console/main.py
@@ -53,6 +53,7 @@ from deluge.ui.console.eventlog import EventLog
 #import screen
 import colors
 from deluge.ui.ui import _UI
+from deluge.ui.console import UI_PATH
 
 log = logging.getLogger(__name__)
 
@@ -62,11 +63,62 @@ class Console(_UI):
 
     def __init__(self):
         super(Console, self).__init__("console")
+        group = optparse.OptionGroup(self.parser, "Console Options","These options control how "
+                                     "the console connects to the daemon.  These options will be "
+                                     "used if you pass a command, or if you have autoconnect "
+                                     "enabled for the console ui.")
+
+        group.add_option("-d","--daemon",dest="daemon_addr",
+                         action="store",type="str",default="127.0.0.1",
+                         help="Set the address of the daemon to connect to."
+                              " [default: %default]")
+        group.add_option("-p","--port",dest="daemon_port",
+                         help="Set the port to connect to the daemon on. [default: %default]",
+                         action="store",type="int",default=58846)
+        group.add_option("-u","--username",dest="daemon_user",
+                         help="Set the username to connect to the daemon with. [default: %default]",
+                         action="store",type="string")
+        group.add_option("-P","--password",dest="daemon_pass",
+                         help="Set the password to connect to the daemon with. [default: %default]",
+                         action="store",type="string")
+        self.parser.add_option_group(group)
+
+        self.cmds = load_commands(os.path.join(UI_PATH, 'commands'))
+        class CommandOptionGroup(optparse.OptionGroup):
+            def __init__(self, parser, title, description=None, cmds = None):
+                optparse.OptionGroup.__init__(self,parser,title,description)
+                self.cmds = cmds
+
+            def format_help(self, formatter):
+                result = formatter.format_heading(self.title)
+                formatter.indent()
+                if self.description:
+                    result += "%s\n"%formatter.format_description(self.description)
+                for cname in self.cmds:
+                    cmd = self.cmds[cname]
+                    if cmd.interactive_only or cname in cmd.aliases: continue
+                    allnames = [cname]
+                    allnames.extend(cmd.aliases)
+                    cname = "/".join(allnames)
+                    result += formatter.format_heading(" - ".join([cname,cmd.__doc__]))
+                    formatter.indent()
+                    result += "%*s%s\n" % (formatter.current_indent, "", cmd.usage)
+                    formatter.dedent()
+                formatter.dedent()
+                return result
+        cmd_group = CommandOptionGroup(self.parser, "Console Commands",
+                                       description="The following commands can be issued at the "
+                                       "command line.  Commands should be quoted, so, for example, "
+                                       "to pause torrent with id 'abc' you would run: '%s "
+                                       "\"pause abc\"'"%os.path.basename(sys.argv[0]),
+                                       cmds=self.cmds)
+        self.parser.add_option_group(cmd_group)
 
     def start(self):
         super(Console, self).start()
-
-        ConsoleUI(self.args)
+        ConsoleUI(self.args,self.cmds,(self.options.daemon_addr,
+                  self.options.daemon_port,self.options.daemon_user,
+                  self.options.daemon_pass))
 
 def start():
     Console().start()
@@ -88,8 +140,61 @@ class OptionParser(optparse.OptionParser):
         raise Exception(msg)
 
 
+class BaseCommand(object):
+
+    usage = 'usage'
+    interactive_only = False
+    option_list = tuple()
+    aliases = []
+
+    def complete(self, text, *args):
+        return []
+    def handle(self, *args, **options):
+        pass
+
+    @property
+    def name(self):
+        return 'base'
+
+    @property
+    def epilog(self):
+        return self.__doc__
+
+    def split(self, text):
+        if deluge.common.windows_check():
+            text = text.replace('\\', '\\\\')
+        return shlex.split(text)
+
+    def create_parser(self):
+        return OptionParser(prog = self.name,
+                            usage = self.usage,
+                            epilog = self.epilog,
+                            option_list = self.option_list)
+
+
+def load_commands(command_dir, exclude=[]):
+    def get_command(name):
+        return getattr(__import__('deluge.ui.console.commands.%s' % name, {}, {}, ['Command']), 'Command')()
+
+    try:
+        commands = []
+        for filename in os.listdir(command_dir):
+            if filename.split('.')[0] in exclude or filename.startswith('_'):
+                continue
+            if not (filename.endswith('.py') or filename.endswith('.pyc')):
+                continue
+            cmd = get_command(filename.split('.')[len(filename.split('.')) - 2])
+            aliases = [ filename.split('.')[len(filename.split('.')) - 2] ]
+            aliases.extend(cmd.aliases)
+            for a in aliases:
+                commands.append((a, cmd))
+        return dict(commands)
+    except OSError, e:
+        return {}
+
+
 class ConsoleUI(component.Component):
-    def __init__(self, args=None):
+    def __init__(self, args=None, cmds = None, daemon = None):
         component.Component.__init__(self, "ConsoleUI", 2)
 
         # keep track of events for the log view
@@ -114,6 +219,18 @@ class ConsoleUI(component.Component):
         if args:
             args = args[0]
             self.interactive = False
+            if not cmds:
+                print "Sorry, couldn't find any commands"
+                return
+            else:
+                self._commands = cmds
+                from commander import Commander
+                cmdr = Commander(cmds)
+                if daemon:
+                    cmdr.exec_args(args,*daemon)
+                else:
+                    cmdr.exec_args(args,None,None,None,None)
+                
 
         self.coreconfig = CoreConfig()
         if self.interactive and not deluge.common.windows_check():
@@ -155,6 +272,44 @@ class ConsoleUI(component.Component):
         reactor.run()
 
 
+    def start(self):
+        # Maintain a list of (torrent_id, name) for use in tab completion
+        if not self.interactive:
+            self.started_deferred = defer.Deferred()
+            self.torrents = []
+            def on_session_state(result):
+                def on_torrents_status(torrents):
+                    for torrent_id, status in torrents.items():
+                        self.torrents.append((torrent_id, status["name"]))
+                    self.started_deferred.callback(True)
+
+                client.core.get_torrents_status({"id": result}, ["name"]).addCallback(on_torrents_status)
+            client.core.get_session_state().addCallback(on_session_state)
+
+            
+    def match_torrent(self, string):
+        """
+        Returns a list of torrent_id matches for the string.  It will search both
+        torrent_ids and torrent names, but will only return torrent_ids.
+
+        :param string: str, the string to match on
+
+        :returns: list of matching torrent_ids. Will return an empty list if
+            no matches are found.
+
+        """
+        ret = []
+        for tid, name in self.torrents:
+            if tid.startswith(string) or name.startswith(string):
+                ret.append(tid)
+
+        return ret
+
+
+    def set_batch_write(self, batch):
+        # only kept for legacy reasons, don't actually do anything
+        pass
+
     def set_mode(self, mode):
         reactor.removeReader(self.screen)
         self.screen = mode
@@ -165,4 +320,7 @@ class ConsoleUI(component.Component):
         component.stop()
 
     def write(self, s):
-        self.events.append(s)
+        if self.interactive:
+            self.events.append(s)
+        else:
+            print colors.strip_colors(s)
