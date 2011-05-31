a96aeed706253d81a93d5b515c9baac2f1987863
Andrew Resch <andrewresch@gmail.com>
2011-05-31 09:56:23 -0700

    Fix #1869: Set the disk io read/write to bypass OS cache in Windows as suggested in http://code.google.com/p/libtorrent/issues/detail?id=166

--- deluge/core/core.py
+++ deluge/core/core.py
@@ -94,6 +94,11 @@ class Core(component.Component):
 
         # Set session settings
         self.settings.send_redundant_have = True
+        if deluge.common.windows_check():
+            self.settings.disk_io_write_mode = \
+                lt.io_buffer_mode_t.disable_os_cache_for_aligned_files
+            self.settings.disk_io_read_mode = \
+                lt.io_buffer_mode_t.disable_os_cache_for_aligned_files
         self.session.set_settings(self.settings)
 
         # Load metadata extension
