66e8b34a54bcfe2b3e964d6ac135036f51ee6175
Pedro Algarvio <pedro@algarvio.me>
2011-06-06 19:33:27 +0100

    GTK UI un-select row on torrentview shutdown and filter change.
    This is specially useful when multiple torrents are selected. This way, the "changed" signal won't be triggered for every row.

--- deluge/ui/gtkui/torrentview.py
+++ deluge/ui/gtkui/torrentview.py
@@ -450,6 +450,7 @@ class TorrentView(listview.ListView, component.Component):
     def stop(self):
         """Stops the torrentview"""
         # We need to clear the liststore
+        self.treeview.unselect_all()
         self.liststore.clear()
         self.prev_status = {}
         self.filter = None
@@ -467,9 +468,12 @@ class TorrentView(listview.ListView, component.Component):
         listview.ListView.save_state(self, "torrentview.state")
 
     def set_filter(self, filter_dict):
-        """Sets filters for the torrentview..
+        """
+        Sets filters for the torrentview..
+
         see: core.get_torrents_status
         """
+        self.treeview.unselect_all()
         search_filter = self.filter and self.filter.get('name', None) or None
         self.filter = dict(filter_dict) #copied version of filter_dict.
         if search_filter and 'name' not in filter_dict:
