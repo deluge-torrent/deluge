fcf26bad459bb0e83f36f4954999466d855f070a
Calum Lind <calumlind+deluge@gmail.com>
2012-02-28 16:43:34 +0000

    Add optparse custom version to prevent unnecessary loading of libtorrent

--- deluge/ui/ui.py
+++ deluge/ui/ui.py
@@ -33,17 +33,22 @@
 #
 #
 
+import os
+import sys
 from optparse import OptionParser, OptionGroup
+
 import deluge.common
 import deluge.configmanager
 import deluge.log
-import os
 
-try:
-    from deluge._libtorrent import lt
-    lt_version = "\nlibtorrent: %s" % lt.version
-except ImportError:
-    lt_version = ""
+def version_callback(option, opt_str, value, parser):
+    print os.path.basename(sys.argv[0]) + ": " + deluge.common.get_version()
+    try:
+        from deluge._libtorrent import lt
+        print "libtorrent: %s" % lt.version
+    except ImportError:
+        pass
+    raise SystemExit
 
 DEFAULT_PREFS = {
     "default_ui": "gtk"
@@ -58,10 +63,9 @@ class _UI(object):
     def __init__(self, name="gtk"):
         self.__name = name
 
-        usage="%prog [options] [actions]",
-
-        self.__parser = OptionParser(version="%prog: " + deluge.common.get_version() + lt_version)
-
+        self.__parser = OptionParser(usage="%prog [options] [actions]")
+        self.__parser.add_option("-v", "--version", action="callback", callback=version_callback,
+            help="Show program's version number and exit")
         group = OptionGroup(self.__parser, "Common Options")
         group.add_option("-c", "--config", dest="config",
             help="Set the config folder location", action="store", type="str")
