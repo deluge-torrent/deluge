14eb3e51b048e61abb36f4288844cf3f197ab409
Calum Lind <calumlind+deluge@gmail.com>
2012-03-13 00:45:58 +0000

    Bring MainWindow to front when opening another instance of gtkui

--- deluge/ui/gtkui/ipcinterface.py
+++ deluge/ui/gtkui/ipcinterface.py
@@ -60,6 +60,7 @@ log = logging.getLogger(__name__)
 class IPCProtocolServer(Protocol):
     def dataReceived(self, data):
         data = rencode.loads(data)
+        component.get("MainWindow").present()
         process_args(data)
 
 class IPCProtocolClient(Protocol):
