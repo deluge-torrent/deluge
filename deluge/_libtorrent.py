74d59eb6ec5d1a19bbed526d072238824a9afb14
Chase Sterling <chase.sterling@gmail.com>
2012-12-03 01:08:31 -0500

    Fix the version check in _libtorrent to work as intended.

--- deluge/_libtorrent.py
+++ deluge/_libtorrent.py
@@ -47,7 +47,7 @@ supports.
 
 REQUIRED_VERSION = "0.14.9.0"
 
-def check_version(LT):
+def check_version(lt):
     from deluge.common import VersionSplit
     if VersionSplit(lt.version) < VersionSplit(REQUIRED_VERSION):
         raise ImportError("This version of Deluge requires libtorrent >=%s!" % REQUIRED_VERSION)
