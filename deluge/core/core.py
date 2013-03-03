deluge-torrent/deluge

commit 5cd86aa5bcbd246f4ed6b4bed026cd72045304a3

Optimizations to daemon and rpc messages.
* Implemented torrent updates with libtorrent post_torrent_updates.
  This required some changes to how core/torrenthandler
  handles get_torrent_status
* Reworked how torrent.py handles torrent info
* Removed some unneeded RPC message requests from client.
* Added tests for some expensive log prints.

deluge/core/core.py
@@ -427,20 +427,27 @@ class Core(component.Component):
         for torrent_id in torrent_ids:
             self.torrentmanager[torrent_id].resume()
 
-    @export
-    def get_torrent_status(self, torrent_id, keys, diff=False):
-        # Build the status dictionary
+    def create_torrent_status(self, torrent_id, torrent_keys, plugin_keys, diff=False, update=True):
         try:
-            status = self.torrentmanager[torrent_id].get_status(keys, diff)
+            if update:
+                status = self.torrentmanager[torrent_id].get_status(torrent_keys, diff)
+            else:
+                status = self.torrentmanager[torrent_id].create_status_dict(torrent_keys, diff)
         except KeyError:
+            import traceback
+            traceback.print_exc()
             # Torrent was probaly removed meanwhile
             return {}
 
-        # Get the leftover fields and ask the plugin manager to fill them
-        leftover_fields = list(set(keys) - set(status.keys()))
-        if len(leftover_fields) > 0:
-            status.update(self.pluginmanager.get_status(torrent_id, leftover_fields))
+        # Ask the plugin manager to fill in the plugin keys
+        if len(plugin_keys) > 0:
+            status.update(self.pluginmanager.get_status(torrent_id, plugin_keys))
         return status
+
+    @export
+    def get_torrent_status(self, torrent_id, keys, diff=False):
+        torrent_keys, plugin_keys = self.torrentmanager.separate_keys(keys, [torrent_id])
+        return self.create_torrent_status(torrent_id, torrent_keys, plugin_keys, diff=diff, update=True)
 
     @export
     def get_torrents_status(self, filter_dict, keys, diff=False):
@@ -449,12 +456,17 @@ class Core(component.Component):
         """
         torrent_ids = self.filtermanager.filter_torrent_ids(filter_dict)
         status_dict = {}.fromkeys(torrent_ids)
+        d = self.torrentmanager.torrents_status_update(torrent_ids, keys, diff=False)
 
-        # Get the torrent status for each torrent_id
-        for torrent_id in torrent_ids:
-            status_dict[torrent_id] = self.get_torrent_status(torrent_id, keys, diff)
-
-        return status_dict
+        def add_plugin_fields(args):
+            status_dict, plugin_keys = args
+            # Ask the plugin manager to fill in the plugin keys
+            if len(plugin_keys) > 0:
+                for key in status_dict.keys():
+                    status_dict[key].update(self.pluginmanager.get_status(key, plugin_keys))
+            return status_dict
+        d.addCallback(add_plugin_fields)
+        return d
 
     @export
     def get_filter_tree(self , show_zero_hits=True, hide_cat=None):

deluge/core/alertmanager.py
@@ -66,12 +66,12 @@ class AlertManager(component.Component):
 
         # handlers is a dictionary of lists {"alert_type": [handler1,h2,..]}
         self.handlers = {}
-
         self.delayed_calls = []
+        self.wait_on_handler = False
 
     def update(self):
         self.delayed_calls = [dc for dc in self.delayed_calls if dc.active()]
-        self.handle_alerts()
+        self.handle_alerts(wait=self.wait_on_handler)
 
     def stop(self):
         for dc in self.delayed_calls:
@@ -122,7 +122,8 @@ class AlertManager(component.Component):
         while alert is not None:
             alert_type = type(alert).__name__
             # Display the alert message
-            log.debug("%s: %s", alert_type, alert.message())
+            if log.isEnabledFor(logging.DEBUG):
+                log.debug("%s: %s", alert_type, alert.message())
             # Call any handlers for this alert type
             if alert_type in self.handlers:
                 for handler in self.handlers[alert_type]:

deluge/core/filtermanager.py
@@ -106,16 +106,15 @@ def tracker_error_filter(torrent_ids, values):
     # If this is a tracker_host, then we need to filter on it
     if values[0] != "Error":
         for torrent_id in torrent_ids:
-            if values[0] == tm[torrent_id].get_status(["tracker_host"])["tracker_host"]:
+            if values[0] == tm[torrent_id].create_status_dict(["tracker_host"])["tracker_host"]:
                 filtered_torrent_ids.append(torrent_id)
         return filtered_torrent_ids
 
     # Check all the torrent's tracker_status for 'Error:' and only return torrent_ids
     # that have this substring in their tracker_status
     for torrent_id in torrent_ids:
-        if _("Error") + ":" in tm[torrent_id].get_status(["tracker_status"])["tracker_status"]:
+        if _("Error") + ":" in tm[torrent_id].create_status_dict(["tracker_host"])["tracker_host"]:
             filtered_torrent_ids.append(torrent_id)
-
     return filtered_torrent_ids
 
 class FilterManager(component.Component):
@@ -192,13 +191,11 @@ class FilterManager(component.Component):
 
         #leftover filter arguments:
         #default filter on status fields.
-        status_func = self.core.get_torrent_status #premature optimalisation..
         for torrent_id in list(torrent_ids):
-            status = status_func(torrent_id, filter_dict.keys()) #status={key:value}
+            status = self.torrents[torrent_id].create_status_dict(filter_dict.keys()) #status={key:value}
             for field, values in filter_dict.iteritems():
                 if (not status[field] in values) and torrent_id in torrent_ids:
                     torrent_ids.remove(torrent_id)
-
         return torrent_ids
 
     def get_filter_tree(self, show_zero_hits=True, hide_cat=None):
@@ -207,17 +204,16 @@ class FilterManager(component.Component):
         for use in sidebar.
         """
         torrent_ids = self.torrents.get_torrent_list()
-        status_func = self.core.get_torrent_status #premature optimalisation..
         tree_keys = list(self.tree_fields.keys())
         if hide_cat:
             for cat in hide_cat:
                 tree_keys.remove(cat)
 
-        items = dict( (field, self.tree_fields[field]()) for field in tree_keys)
+        torrent_keys, plugin_keys = self.torrents.separate_keys(tree_keys, torrent_ids)
+        items = dict((field, self.tree_fields[field]()) for field in tree_keys)
 
-        #count status fields.
         for torrent_id in list(torrent_ids):
-            status = status_func(torrent_id, tree_keys) #status={key:value}
+            status = self.core.create_torrent_status(torrent_id, torrent_keys, plugin_keys, update=False) #status={key:value}
             for field in tree_keys:
                 value = status[field]
                 items[field][value] = items[field].get(value, 0) + 1
@@ -264,9 +260,8 @@ class FilterManager(component.Component):
             del self.tree_fields[field]
 
     def filter_state_active(self, torrent_ids):
-        get_status = self.core.get_torrent_status
         for torrent_id in list(torrent_ids):
-            status = get_status(torrent_id, ["download_payload_rate", "upload_payload_rate"])
+            status = self.torrents[torrent_id].create_status_dict(["download_payload_rate", "upload_payload_rate"])
             if status["download_payload_rate"] or status["upload_payload_rate"]:
                 pass #ok
             else:

deluge/core/torrentmanager.py
@@ -41,9 +41,11 @@ import os
 import shutil
 import operator
 import logging
+import time
 
 from twisted.internet.task import LoopingCall
 from twisted.internet.defer import Deferred, DeferredList
+from twisted.internet import reactor
 
 from deluge._libtorrent import lt
 
@@ -157,6 +159,10 @@ class TorrentManager(component.Component):
         # Keeps track of resume data
         self.resume_data = {}
 
+        self.torrents_status_requests = []
+        self.status_dict = {}
+        self.state_update_alert_start = None
+
         # Register set functions
         self.config.register_set_function("max_connections_per_torrent",
             self.on_set_max_connections_per_torrent)
@@ -200,6 +206,8 @@ class TorrentManager(component.Component):
             self.on_alert_file_error)
         self.alerts.register_handler("file_completed_alert",
             self.on_alert_file_completed)
+        self.alerts.register_handler("state_update_alert",
+            self.state_update_alert)
 
     def start(self):
         # Get the pluginmanager reference
@@ -286,7 +294,8 @@ class TorrentManager(component.Component):
         torrent_info = None
         # Get the torrent data from the torrent file
         try:
-            log.debug("Attempting to create torrent_info from %s", filepath)
+            if log.isEnabledFor(logging.DEBUG):
+                log.debug("Attempting to create torrent_info from %s", filepath)
             _file = open(filepath, "rb")
             torrent_info = lt.torrent_info(lt.bdecode(_file.read()))
             _file.close()
@@ -321,7 +330,6 @@ class TorrentManager(component.Component):
     def add(self, torrent_info=None, state=None, options=None, save_state=True,
             filedump=None, filename=None, magnet=None, resume_data=None, owner=None):
         """Add a torrent to the manager and returns it's torrent_id"""
-
         if owner is None:
             owner = component.get("RPCServer").get_session_user()
             if not owner:
@@ -331,7 +339,6 @@ class TorrentManager(component.Component):
             log.debug("You must specify a valid torrent_info, torrent state or magnet.")
             return
 
-        log.debug("torrentmanager.add")
         add_torrent_params = {}
 
         if filedump is not None:
@@ -439,8 +446,8 @@ class TorrentManager(component.Component):
 
             add_torrent_params["ti"] = torrent_info
 
-        #log.info("Adding torrent: %s", filename)
-        log.debug("options: %s", options)
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("options: %s", options)
 
         # Set the right storage_mode
         if options["compact_allocation"]:
@@ -474,7 +481,8 @@ class TorrentManager(component.Component):
             component.resume("AlertManager")
             return
 
-        log.debug("handle id: %s", str(handle.info_hash()))
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("handle id: %s", str(handle.info_hash()))
         # Set auto_managed to False because the torrent is paused
         handle.auto_managed(False)
         # Create a Torrent object
@@ -485,6 +493,7 @@ class TorrentManager(component.Component):
         if not account_exists:
             owner = 'localclient'
         torrent = Torrent(handle, options, state, filename, magnet, owner)
+
         # Add the torrent object to the dictionary
         self.torrents[torrent.torrent_id] = torrent
         if self.config["queue_new_to_top"]:
@@ -531,10 +540,14 @@ class TorrentManager(component.Component):
         component.get("EventManager").emit(
             TorrentAddedEvent(torrent.torrent_id, from_state)
         )
-        log.info("Torrent %s from user \"%s\" %s",
-                 torrent.get_status(["name"])["name"],
-                 torrent.get_status(["owner"])["owner"],
-                 (from_state and "loaded" or "added"))
+
+        if log.isEnabledFor(logging.INFO):
+            name_and_owner = torrent.get_status(["name", "owner"])
+            log.info("Torrent %s from user \"%s\" %s" % (
+                    name_and_owner["name"],
+                    name_and_owner["owner"],
+                    from_state and "loaded" or "added")
+                     )
         return torrent.torrent_id
 
     def load_torrent(self, torrent_id):
@@ -646,19 +659,25 @@ class TorrentManager(component.Component):
 
         # Try to use an old state
         try:
-            state_tmp = TorrentState()
-            if dir(state.torrents[0]) != dir(state_tmp):
-                for attr in (set(dir(state_tmp)) - set(dir(state.torrents[0]))):
-                    for s in state.torrents:
-                        setattr(s, attr, getattr(state_tmp, attr, None))
+            if len(state.torrents) > 0:
+                state_tmp = TorrentState()
+                if dir(state.torrents[0]) != dir(state_tmp):
+                    for attr in (set(dir(state_tmp)) - set(dir(state.torrents[0]))):
+                        for s in state.torrents:
+                            setattr(s, attr, getattr(state_tmp, attr, None))
         except Exception, e:
             log.warning("Unable to update state file to a compatible version: %s", e)
+            import traceback
+            print traceback.format_exc()
 
         # Reorder the state.torrents list to add torrents in the correct queue
         # order.
         state.torrents.sort(key=operator.attrgetter("queue"), reverse=self.config["queue_new_to_top"])
-
         resume_data = self.load_resume_data_file()
+
+        # Tell alertmanager to wait for the handlers while adding torrents.
+        # This speeds up startup loading the torrents by quite a lot for some reason (~40%)
+        self.alerts.wait_on_handler = True
 
         for torrent_state in state.torrents:
             try:
@@ -666,8 +685,11 @@ class TorrentManager(component.Component):
                          resume_data=resume_data.get(torrent_state.torrent_id))
             except AttributeError, e:
                 log.error("Torrent state file is either corrupt or incompatible! %s", e)
+                import traceback
+                traceback.print_exc()
                 break
 
+        self.alerts.wait_on_handler = False
 
         if lt.version_minor < 16:
             log.debug("libtorrent version is lower than 0.16. Start looping "
@@ -913,7 +935,8 @@ class TorrentManager(component.Component):
             self.save_resume_data((torrent_id, ))
 
     def on_alert_torrent_paused(self, alert):
-        log.debug("on_alert_torrent_paused")
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("on_alert_torrent_paused")
         try:
             torrent = self.torrents[str(alert.handle.info_hash())]
             torrent_id = str(alert.handle.info_hash())
@@ -930,7 +953,8 @@ class TorrentManager(component.Component):
             self.save_resume_data((torrent_id,))
 
     def on_alert_torrent_checked(self, alert):
-        log.debug("on_alert_torrent_checked")
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("on_alert_torrent_checked")
         try:
             torrent = self.torrents[str(alert.handle.info_hash())]
         except:
@@ -947,7 +971,8 @@ class TorrentManager(component.Component):
         torrent.update_state()
 
     def on_alert_tracker_reply(self, alert):
-        log.debug("on_alert_tracker_reply: %s", alert.message().decode("utf8"))
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("on_alert_tracker_reply: %s", alert.message().decode("utf8"))
         try:
             torrent = self.torrents[str(alert.handle.info_hash())]
         except:
@@ -964,7 +989,8 @@ class TorrentManager(component.Component):
             torrent.scrape_tracker()
 
     def on_alert_tracker_announce(self, alert):
-        log.debug("on_alert_tracker_announce")
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("on_alert_tracker_announce")
         try:
             torrent = self.torrents[str(alert.handle.info_hash())]
         except:
@@ -1016,7 +1042,8 @@ class TorrentManager(component.Component):
         component.get("EventManager").emit(TorrentResumedEvent(torrent_id))
 
     def on_alert_state_changed(self, alert):
-        log.debug("on_alert_state_changed")
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("on_alert_state_changed")
         try:
             torrent_id = str(alert.handle.info_hash())
             torrent = self.torrents[torrent_id]
@@ -1036,7 +1063,8 @@ class TorrentManager(component.Component):
             component.get("EventManager").emit(TorrentStateChangedEvent(torrent_id, torrent.state))
 
     def on_alert_save_resume_data(self, alert):
-        log.debug("on_alert_save_resume_data")
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug("on_alert_save_resume_data")
         torrent_id = str(alert.handle.info_hash())
 
         if torrent_id in self.torrents:
@@ -1096,3 +1124,62 @@ class TorrentManager(component.Component):
             return
         component.get("EventManager").emit(
             TorrentFileCompletedEvent(torrent_id, alert.index))
+
+    def separate_keys(self, keys, torrent_ids):
+        """Separates the input keys into keys for the Torrent class
+        and keys for plugins.
+        """
+        if not self.torrents:
+            return [], []
+        for torrent_id in torrent_ids:
+            if torrent_id in self.torrents:
+                status_keys = self.torrents[torrent_id].status_funcs.keys()
+                leftover_keys = list(set(keys) - set(status_keys))
+                torrent_keys = list(set(keys) - set(leftover_keys))
+                return torrent_keys, leftover_keys
+
+    def state_update_alert(self, alert):
+        self.state_update_alert_start = time.time()
+        log.debug("on_status_notification: %s", alert.message())
+
+        for s in alert.status:
+            torrent_id = str(s.info_hash)
+            if torrent_id in self.torrents:
+                self.torrents[torrent_id].update_status(s)
+
+        self.handle_torrents_status_callback(self.torrents_status_requests.pop())
+
+    def handle_torrents_status_callback(self, status_request):
+        d, torrent_ids, keys, diff = status_request
+        status_dict = {}.fromkeys(torrent_ids)
+        torrent_keys, plugin_keys = self.separate_keys(keys, torrent_ids)
+
+        # Get the torrent status for each torrent_id
+        for torrent_id in torrent_ids:
+            if not torrent_id in self.torrents:
+                # This happens sometimes, but why? Unfixed bug?
+                # Without this test it gives a KeyError.
+                print "Missing torrent id:", torrent_id
+                del status_dict[torrent_id]
+            else:
+                status_dict[torrent_id] = self.torrents[torrent_id].create_status_dict(torrent_keys, diff)
+        self.status_dict = status_dict
+        d.callback((status_dict, plugin_keys))
+
+    def torrents_status_update(self, torrent_ids, keys, diff=False):
+        """
+        returns all torrents , optionally filtered by filter_dict.
+        If the torrent states were updated recently (less than two seconds ago,
+        post_torrent_updates is not called. Instead the cached state is used.
+        """
+        d = Deferred()
+        now = time.time()
+        # Less than two seconds since last time the torrent states were updated
+        if self.state_update_alert_start and \
+                (now - self.state_update_alert_start) < 2:
+            reactor.callLater(0, self.handle_torrents_status_callback, (d, torrent_ids, keys, diff))
+        else:
+            # Ask libtorrent for status update
+            self.torrents_status_requests.insert(0, (d, torrent_ids, keys, diff))
+            self.session.post_torrent_updates()
+        return d

deluge/ui/gtkui/statusbar.py
@@ -194,16 +194,16 @@ class StatusBar(component.Component):
 
         self.health = False
 
+        def update_config_values(configs):
+            self._on_max_connections_global(configs["max_connections_global"])
+            self._on_max_download_speed(configs["max_download_speed"])
+            self._on_max_upload_speed(configs["max_upload_speed"])
+            self._on_dht(configs["dht"])
         # Get some config values
-        client.core.get_config_value(
-            "max_connections_global").addCallback(self._on_max_connections_global)
-        client.core.get_config_value(
-            "max_download_speed").addCallback(self._on_max_download_speed)
-        client.core.get_config_value(
-            "max_upload_speed").addCallback(self._on_max_upload_speed)
-        client.core.get_config_value("dht").addCallback(self._on_dht)
-
-        self.send_status_request()
+        client.core.get_config_values(["max_connections_global", "max_download_speed",
+                                  "max_upload_speed", "dht"]).addCallback(update_config_values)
+        # Called from update a few milliseconds later
+        #self.send_status_request()
 
     def stop(self):
         # When stopped, we just show the not connected thingy

deluge/ui/gtkui/systemtray.py
@@ -172,11 +172,12 @@ class SystemTray(component.Component):
             self.build_tray_bwsetsubmenu()
 
             # Get some config values
-            client.core.get_config_value(
-                "max_download_speed").addCallback(self._on_max_download_speed)
-            client.core.get_config_value(
-                "max_upload_speed").addCallback(self._on_max_upload_speed)
-            self.send_status_request()
+            def update_config_values(configs):
+                self._on_max_download_speed(configs["max_download_speed"])
+                self._on_max_upload_speed(configs["max_upload_speed"])
+            client.core.get_config_values(["max_download_speed", "max_upload_speed"]).addCallback(update_config_values)
+            # This is called from update a few milliseconds later, so this line can be deleted
+            #self.send_status_request()
 
     def start(self):
         self.__start()

deluge/ui/sessionproxy.py
@@ -85,11 +85,14 @@ class SessionProxy(component.Component):
             # core if it's not going to be used. Additional status fields
             # will be queried later, for example, when viewing the status tab
             # of a torrent.
-            inital_keys = [
-                'queue', 'state', 'name', 'total_wanted', 'progress', 'state',
-                'download_payload_rate', 'upload_payload_rate', 'eta', 'owner'
-            ]
-            self.get_torrents_status({'id': torrent_ids}, inital_keys)
+            #inital_keys = [
+            #    'queue', 'state', 'name', 'total_wanted', 'progress', 'state',
+            #    'download_payload_rate', 'upload_payload_rate', 'eta', 'owner'
+            #]
+
+            #The the torrents status will be requested by torrentview, so this
+            #only causes a second request for the same data withing a few miliseconds
+            #self.get_torrents_status({'id': torrent_ids}, inital_keys)
         return client.core.get_session_state().addCallback(on_get_session_state)
 
     def stop(self):

commit 2a3eb7b70cb5a71c741ffe105b51ee17cec11589

Fixes bug in core.get_torrents_status where diff was always False
Fixes bug introduced in 5cd86aa5bcbd246f4ed6b4bed026cd72045304a3 where the diff
argument passed to torrentmanager.torrents_status_update is always False
instead of the value passed into get_torrents_status. This causes the sessionproxy
not to function properly which in turn increases CPU usage in the UIs.

deluge/core/core.py
@@ -453,7 +453,7 @@ class Core(component.Component):
         """
         torrent_ids = self.filtermanager.filter_torrent_ids(filter_dict)
         status_dict = {}.fromkeys(torrent_ids)
-        d = self.torrentmanager.torrents_status_update(torrent_ids, keys, diff=False)
+        d = self.torrentmanager.torrents_status_update(torrent_ids, keys, diff=diff)
 
         def add_plugin_fields(args):
             status_dict, plugin_keys = args