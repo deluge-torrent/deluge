21691c5cc169e9ca4700f42d0229b260a4c45cd4
Calum Lind <calumlind+deluge@gmail.com>
2014-07-11 18:59:52 +0100

    [Extractor] Replace module which with twisted.python.procutils.which

--- deluge/plugins/Extractor/deluge/plugins/extractor/core.py
+++ deluge/plugins/Extractor/deluge/plugins/extractor/core.py
@@ -41,13 +41,13 @@ import os
 import logging
 
 from twisted.internet.utils import getProcessValue
+from twisted.python.procutils import which
 
 from deluge.plugins.pluginbase import CorePluginBase
 import deluge.component as component
 import deluge.configmanager
 from deluge.core.rpcserver import export
 from deluge.common import windows_check
-from extractor.which import which
 
 log = logging.getLogger(__name__)
 
