9f5e5b02d5f4925bc402e615d1bc4f8758261d16
doadin <tbkizle@gmail.com>
2015-03-22 20:23:04 -0400

    fix keysyms import

--- deluge/ui/gtkui/path_combo_chooser.py
+++ deluge/ui/gtkui/path_combo_chooser.py
@@ -13,7 +13,7 @@ from __future__ import print_function
 import os
 
 from gi.repository import GObject, Gtk, Gdk
-from Gtk import keysyms
+from gi.overrides import keysyms
 
 from deluge.common import resource_filename
 from deluge.path_chooser_common import get_completion_paths
