7ab5164bcb67a8369f126ceda9d737c9c280bf15
doadin <tbkizle@gmail.com>
2015-08-18 14:04:48 -0400

    add removed code comment with fixme
    "File "C:\Python27\lib\site-packages\gi\overrides\GObject.py", line 545,
    in _un
    supported_data_method
    raise RuntimeError('Data access methods are unsupported. '
    RuntimeError: Data access methods are unsupported. Use normal Python
    attributes instead"
    
    same for all set_data lines

--- deluge/plugins/Blocklist/deluge/plugins/blocklist/gtkui.py
+++ deluge/plugins/Blocklist/deluge/plugins/blocklist/gtkui.py
@@ -188,6 +188,8 @@ class GtkUI(GtkPluginBase):
         self.whitelist_model = Gtk.ListStore(str, bool)
         renderer = Gtk.CellRendererText()
         renderer.connect("edited", self.on_cell_edited, self.whitelist_model)
+        # FIXME
+        # renderer.set_data("ip", 0)
 
         column = Gtk.TreeViewColumn("IPs", renderer, text=0, editable=1)
         column.set_expand(True)
