1fff93ea906ab1603af98aa59fd61b08c55e31ca
doadin <tbkizle@gmail.com>
2015-08-18 12:26:53 -0400

    GTKUI mainwindow add fix from cas

--- deluge/ui/gtkui/mainwindow.py
+++ deluge/ui/gtkui/mainwindow.py
@@ -294,7 +294,7 @@ class MainWindow(component.Component):
         self.config["window_pane_position"] = self.vpaned.get_position()
 
     def on_drag_data_received_event(self, widget, drag_context, x, y, selection_data, info, timestamp):
-        log.debug("Selection(s) dropped on main window %s", selection_data)  # fixed AttributeError: 'SelectionData' object has no attribute 'data'
+        log.debug("Selection(s) dropped on main window %s", selection_data.get_text())
         if selection_data.get_uris():
             process_args(selection_data.get_uris())
         else:
