bb5a9a040f4ea6b57cb4fec241077acb0f4504a2
doadin <tbkizle@gmail.com>
2015-08-18 12:54:10 -0400

    GTKUI filtertreeview remove commented code

--- deluge/ui/gtkui/filtertreeview.py
+++ deluge/ui/gtkui/filtertreeview.py
@@ -247,10 +247,6 @@ class FilterTreeView(component.Component):
         return self.get_transparent_pix(16, 16)
 
     def get_transparent_pix(self, width, height):
-        # pix = GdkPixbuf.Pixbuf(GdkPixbuf.Colorspace.RGB, True, 8, width, height)
-        # pix.fill(0x0000000)
-        # return pix
-
         # Should really give back a properly size pixbuf...
         from deluge.ui.gtkui import torrentview_data_funcs as funcs
         return funcs.icon_empty
