9662ccf486ee845948b54ab4b86801845e625675
Calum Lind <calumlind+deluge@gmail.com>
2015-08-25 11:03:20 +0100

    Use just Taiwan in countries list

--- deluge/ui/countries.py
+++ deluge/ui/countries.py
@@ -221,7 +221,7 @@ COUNTRIES = {
     'SE': _('Sweden'),
     'CH': _('Switzerland'),
     'SY': _('Syrian Arab Republic'),
-    'TW': _('Taiwan, Province of China'),
+    'TW': _('Taiwan'),
     'TJ': _('Tajikistan'),
     'TZ': _('Tanzania, United Republic of'),
     'TH': _('Thailand'),
