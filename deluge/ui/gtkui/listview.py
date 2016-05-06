deluge-torrent/deluge

commit 1a2ff9b089cd23efdef5c8d7dbed84f8ca300d1c

[Lint] Fix and remove old-style-class from ignore warnings

deluge/ui/gtkui/listview.py
@@ -23,7 +23,7 @@ signal_new('button-press-event', gtk.TreeViewColumn, SIGNAL_RUN_LAST, TYPE_NONE,
 log = logging.getLogger(__name__)
 
 
-class ListViewColumnState:
+class ListViewColumnState(object):
     """Used for saving/loading column state"""
     def __init__(self, name, position, width, visible, sort, sort_order):
         self.name = name
@@ -34,13 +34,13 @@ class ListViewColumnState:
         self.sort_order = sort_order
 
 
-class ListView:
+class ListView(object):
     """ListView is used to make custom GtkTreeViews.  It supports the adding
     and removing of columns, creating a menu for a column toggle list and
     support for 'status_field's which are used while updating the columns data.
     """
 
-    class ListViewColumn:
+    class ListViewColumn(object):
         """Holds information regarding a column in the ListView"""
         def __init__(self, name, column_indices):
             # Name is how a column is identified and is also the header

.pylintrc
@@ -62,15 +62,14 @@ confidence=
 #
 # Arranged by category: Convention, Error, Information, Refactor, Warning.
 # Category per line (wrapped categories are indented) using symbolic names instead of ids.
-disable=missing-docstring, invalid-name, old-style-class, bad-continuation, wrong-import-position,
+disable=missing-docstring, invalid-name, bad-continuation, wrong-import-position,
         wrong-import-order,
     no-member, not-callable, no-name-in-module,
     locally-disabled,
     R,
     unused-argument, fixme, protected-access, import-error, unused-variable,
         global-statement, attribute-defined-outside-init, arguments-differ,
-        no-init, super-init-not-called,
-        broad-except
+        no-init, super-init-not-called, broad-except
 
 [REPORTS]
 

deluge/__rpcapi.py
@@ -13,7 +13,7 @@ from deluge.core.core import Core
 from deluge.core.daemon import Daemon
 
 
-class RpcApi:
+class RpcApi(object):
     pass
 
 

deluge/configmanager.py
@@ -17,7 +17,7 @@ from deluge.config import Config
 log = logging.getLogger(__name__)
 
 
-class _ConfigManager:
+class _ConfigManager(object):
     def __init__(self):
         log.debug("ConfigManager started..")
         self.config_files = {}

deluge/core/torrentmanager.py
@@ -34,8 +34,11 @@ from deluge.event import (PreTorrentRemovedEvent, SessionStartedEvent, TorrentAd
 log = logging.getLogger(__name__)
 
 
-class TorrentState:
-    """Create a torrent state"""
+class TorrentState:  # pylint: disable=old-style-class
+    """
+    Create a torrent state
+    This must be old style class to avoid breaking torrent.state file
+    """
     def __init__(self,
                  torrent_id=None,
                  filename=None,
@@ -72,8 +75,10 @@ class TorrentState:
             setattr(self, key, value)
 
 
-class TorrentManagerState:
-    """TorrentManagerState holds a list of TorrentState objects"""
+class TorrentManagerState:  # pylint: disable=old-style-class
+    """TorrentManagerState holds a list of TorrentState objects
+    This must be old style class to avoid breaking torrent.state file
+    """
     def __init__(self):
         self.torrents = []
 

deluge/plugins/Stats/deluge/plugins/stats/graph.py
@@ -56,7 +56,7 @@ def change_opacity(color, opactiy):
     return tuple(color)
 
 
-class Graph:
+class Graph(object):
     def __init__(self):
         self.width = 100
         self.height = 100

deluge/plugins/Stats/deluge/plugins/stats/tests/test_stats.py
@@ -90,7 +90,7 @@ class StatsTestCase(BaseTestCase):
         TorrentDetails()
         Preferences()
 
-        class FakeFile:
+        class FakeFile(object):
             def __init__(self):
                 self.data = []
 

deluge/ui/console/modes/input_popup.py
@@ -24,7 +24,7 @@ from deluge.ui.console.modes.popup import ALIGN, Popup
 log = logging.getLogger(__name__)
 
 
-class InputField:
+class InputField(object):
     depend = None
     # render the input.  return number of rows taken up
 

commit c7b272561ee3c9c96cf96c776bb2f418bc3aed8f

[#2827] [GTKUI] Fix issue with loading GTKUI columns state
commit 1a2ff9b089cd2 introduced a bug when loading GTKUI
column states due to changing ListViewColumnState class type.

Fixed by reverting ListViewColumnState to old style class

deluge/ui/gtkui/listview.py
@@ -23,8 +23,12 @@ signal_new('button-press-event', gtk.TreeViewColumn, SIGNAL_RUN_LAST, TYPE_NONE,
 log = logging.getLogger(__name__)
 
 
-class ListViewColumnState(object):
-    """Used for saving/loading column state"""
+class ListViewColumnState:  # pylint: disable=old-style-class
+    """
+    Class used for saving/loading column state.
+
+    Note: This must be old style class to avoid breaking existing user state file.
+    """
     def __init__(self, name, position, width, visible, sort, sort_order):
         self.name = name
         self.position = position