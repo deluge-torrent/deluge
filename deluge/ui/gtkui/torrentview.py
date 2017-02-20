bcf0fe4a61dbcbcad5d9eb867ff49e6e1f6f90f2
Calum Lind <calumlind+deluge@gmail.com>
2017-02-20 10:02:54 +0000

    [#2957] [GTKUI] Fix AttributeError in torrentview column sort

--- deluge/ui/gtkui/torrentview.py
+++ deluge/ui/gtkui/torrentview.py
@@ -176,8 +176,13 @@ def str_nocase_sort(model, iter1, iter2, data):
     uses ISO 14651.
 
     """
-    v1 = model[iter1][data].lower()
-    v2 = model[iter2][data].lower()
+    try:
+        v1 = model[iter1][data].lower()
+        v2 = model[iter2][data].lower()
+    except AttributeError:
+        # Catch None type for value.
+        v1 = model[iter1][data]
+        v2 = model[iter2][data]
     return strcoll(v1, v2)
 
 def queue_peer_seed_sort_function(v1, v2):
