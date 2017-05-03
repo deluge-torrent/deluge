a6e8ac8725c2be28679e26b7c6674aad339338b1
Calum Lind <calumlind+deluge@gmail.com>
2017-05-03 15:59:52 +0100

    Update Changelog and bump release to 1.3.15

--- setup.py
+++ setup.py
@@ -535,7 +535,7 @@ if windows_check():
 # Main setup
 setup(
     name = "deluge",
-    version = "1.3.14",
+    version = "1.3.15",
     fullname = "Deluge Bittorrent Client",
     description = "Bittorrent Client",
     author = "Andrew Resch, Damien Churchill",
