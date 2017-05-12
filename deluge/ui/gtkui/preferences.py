38d7b7cdfde3c50d6263602ffb03af92fcbfa52e
Calum Lind <calumlind+deluge@gmail.com>
2017-05-13 00:05:48 +0100

    [GTKUI] Fix keyerror showing prefs
     * Fix the single_proxy ui to True as unlikely any users using <=0.15
       and need to have different proxy type settings.

--- deluge/ui/gtkui/preferences.py
+++ deluge/ui/gtkui/preferences.py
@@ -318,11 +318,8 @@ class Preferences(component.Component):
             }
             # Add proxy stuff
 
-            # Display workaround for single proxy in libtorrent >v0.16
-            try:
-                lt_single_proxy = component.get("PreferencesManager").LT_SINGLE_PROXY
-            except AttributeError:
-                lt_single_proxy = False
+            # Display workaround for single proxy in libtorrent >= v0.16
+            lt_single_proxy = True
 
             for t in ("peer", "web_seed", "tracker", "dht"):
                 if lt_single_proxy and not t == "peer":
