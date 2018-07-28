3fc97672de5ef74ec5281aab3d1d0d68b120ee38
Calum Lind <calumlind+deluge@gmail.com>
2018-07-28 08:14:25 +0100

    Fix the docs run failing on Travis
    Likely that the deprecation warning from cryptography is causing the
    setup.py sphinx build command to return an error so the tox/travis job
    is marked as failing. Changing to calling the sphinx-build command
    directly solves this.
    
    Also updated the sphinx config for built-in napoleon and faster builds
    using jobs option.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,7 +73,12 @@ for mod_name in MOCK_MODULES:
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinxcontrib.napoleon', 'sphinx.ext.coverage']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.coverage',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
