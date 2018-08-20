9f113eab230fc59b3c84f8b99a4a3bb2a1b36175
hugosenari <hugosenari@gmail.com>
2018-08-20 01:29:21 -0300

    [GTK3] Fix ImportError has no attribute message
    Python 3 exception objects now use `msg` but casting with `str()` is
    better.

--- deluge/ui/gtk3/gtkui.py
+++ deluge/ui/gtk3/gtkui.py
@@ -331,7 +331,7 @@ class GtkUI(object):
                 'To use Standalone mode, stop local daemon and restart Deluge.'
             )
         except ImportError as ex:
-            if 'No module named libtorrent' in ex.message:
+            if 'No module named libtorrent' in str(ex):
                 err_msg = _(
                     'Only Thin Client mode is available because libtorrent is not installed.\n'
                     'To use Standalone mode, please install libtorrent package.'
