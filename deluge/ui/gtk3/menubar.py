535fda90e3999d1900b6c2c4fbec2bfc26989f21
hugosenari <hugosenari@gmail.com>
2018-08-20 07:13:16 -0300

    [GTK3] Use core.pause_torrents with list of torrents
    Use the new api method.

--- deluge/ui/gtk3/menubar.py
+++ deluge/ui/gtk3/menubar.py
@@ -274,11 +274,13 @@ class MenuBar(component.Component):
     # Torrent Menu #
     def on_menuitem_pause_activate(self, data=None):
         log.debug('on_menuitem_pause_activate')
-        client.core.pause_torrent(component.get('TorrentView').get_selected_torrents())
+        client.core.pause_torrents(component.get('TorrentView').get_selected_torrents())
 
     def on_menuitem_resume_activate(self, data=None):
         log.debug('on_menuitem_resume_activate')
-        client.core.resume_torrent(component.get('TorrentView').get_selected_torrents())
+        client.core.resume_torrents(
+            component.get('TorrentView').get_selected_torrents()
+        )
 
     def on_menuitem_updatetracker_activate(self, data=None):
         log.debug('on_menuitem_updatetracker_activate')
