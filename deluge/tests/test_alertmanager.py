98075b91902749b522a618bca00ae6a936cefd14
DjLegolas <DjLegolas@users.noreply.github.com>
2018-10-22 03:24:20 +0300

    [Core] Call wait_for_alert in a thread
    This spawns a thread in alertmanager to call wait_for_alert in a thread.
    This reduces latency to deluge responding to events.
    And removes all `hasattr` checks in Component

--- deluge/tests/test_alertmanager.py
+++ deluge/tests/test_alertmanager.py
@@ -4,9 +4,37 @@
 # See LICENSE for more details.
 #
 
+from twisted.internet.defer import Deferred
+
 import deluge.component as component
 from deluge.conftest import BaseTestCase
 from deluge.core.core import Core
+
+
+class DummyAlert1:
+    def __init__(self):
+        self.message = '1'
+
+
+class DummyAlert2:
+    def __init__(self):
+        self.message = '2'
+
+
+class SessionMock:
+    def __init__(self):
+        self.alerts = []
+
+    def set_alerts(self, value):
+        self.alerts = [DummyAlert1(), DummyAlert2()]
+
+    def wait_for_alert(self, timeout):
+        return self.alerts[0] if len(self.alerts) > 0 else None
+
+    def pop_alerts(self):
+        alerts = self.alerts
+        self.alerts = []
+        return alerts
 
 
 class TestAlertManager(BaseTestCase):
@@ -14,6 +42,7 @@ class TestAlertManager(BaseTestCase):
         self.core = Core()
         self.core.config.config['lsd'] = False
         self.am = component.get('AlertManager')
+        self.am.session = SessionMock()
         return component.start(['AlertManager'])
 
     def tear_down(self):
@@ -25,6 +54,34 @@ class TestAlertManager(BaseTestCase):
 
         self.am.register_handler('dummy_alert', handler)
         assert self.am.handlers['dummy_alert'] == [handler]
+
+    def test_pop_alert(self):
+        def dummy_alert_1_handler(alert):
+            d.addCallback(self.assertEqual, alert.message, '1')
+            d.callback(None)
+
+        self.am.register_handler('DummyAlert1', dummy_alert_1_handler)
+        self.am.session.set_alerts()
+
+        d = Deferred()
+        return d
+
+    def test_pause_not_pop_alert(self):
+        def on_pause(value):
+            self.am.register_handler('DummyAlert1', dummy_alert_1_handler)
+            d.addCallback(check_alert)
+
+        def check_alert(value):
+            event = self.am._event.isSet()
+            self.assertFalse(event)
+
+        def dummy_alert_1_handler(alert):
+            self.fail()
+
+        d = component.pause(['AlertManager'])
+        d.addCallback(self.am.session.set_alerts)
+        d.addCallback(on_pause)
+        return d
 
     def test_deregister_handler(self):
         def handler(alert):
