cas--/deluge-1

[GTK3] Fix GObject deprecation warnings
GObject.idle_add is deprecated using GLib.idle_add instead
GObject.timeout_add is deprecated using GLib.timeout_add instead
GObject.SIGNAL_RUN_LAST is deprecated; use GObject.SignalFlags.RUN_LAST instead
GObject.GError is deprecated; use GLib.GError instead
GObject.timeout_add is deprecated use GLib.timeout_add instead.
ListStore(str, str) using unicode_literals get_value return utf8.

deluge/ui/gtk3/listview.py
@@ -13,14 +13,14 @@ import logging
 
 from gi.repository import GObject, Gtk
 from gi.repository.Gdk import Event  # pylint: disable=ungrouped-imports
-from gi.repository.GObject import SIGNAL_RUN_LAST, TYPE_NONE, signal_new
+from gi.repository.GObject import SignalFlags, TYPE_NONE, signal_new
 
 from deluge.common import decode_bytes
 
 from .common import load_pickled_state_file, save_pickled_state_file
 
 # FIXME: ?
-signal_new('button-press-event', Gtk.TreeViewColumn, SIGNAL_RUN_LAST, TYPE_NONE, (Event,))
+signal_new('button-press-event', Gtk.TreeViewColumn, SignalFlags.RUN_LAST, TYPE_NONE, (Event,))
 
 log = logging.getLogger(__name__)
 

deluge-torrent/deluge

deluge/ui/gtk3/listview.py
@@ -13,7 +13,7 @@ import logging
 
 from gi.repository import GObject, Gtk
 from gi.repository.Gdk import Event  # pylint: disable=ungrouped-imports
-from gi.repository.GObject import SIGNAL_RUN_LAST, TYPE_NONE, signal_new
+from gi.repository.GObject import TYPE_NONE, SignalFlags, signal_new
 
 from deluge.common import decode_bytes
 
@@ -21,7 +21,7 @@ from .common import load_pickled_state_file, save_pickled_state_file
 
 # FIXME: ?
 signal_new(
-    'button-press-event', Gtk.TreeViewColumn, SIGNAL_RUN_LAST, TYPE_NONE, (Event,)
+    'button-press-event', Gtk.TreeViewColumn, SignalFlags.RUN_LAST, TYPE_NONE, (Event,)
 )
 
 log = logging.getLogger(__name__)