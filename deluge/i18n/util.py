1c0db7170bc01cc9e1076f6b5a8d60de64e3eddc
neeshy <neeshy@tfwno.gf>
2020-04-18 23:23:51 -0400

    Fix warning related to gettext

--- deluge/i18n/util.py
+++ deluge/i18n/util.py
@@ -114,7 +114,7 @@ def setup_translation():
         # Workaround for Python 2 unicode gettext (keyword removed in Py3).
         kwargs = {} if not deluge.common.PY2 else {'unicode': True}
 
-        gettext.install(I18N_DOMAIN, translations_path, names='ngettext', **kwargs)
+        gettext.install(I18N_DOMAIN, translations_path, names=['ngettext'], **kwargs)
         builtins.__dict__['_n'] = builtins.__dict__['ngettext']
 
         libintl = None
