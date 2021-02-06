092d07b68e48fb8480cb068bc6558ba3b269fb95
Calum Lind <calumlind+deluge@gmail.com>
2021-02-06 17:21:11 +0000

    [#3337|Core] Fix lt listen_interfaces not comma-separated
    A typo meant that the interfaces supplied to libtorrent were not
    comma-separated.
    
    Refs: https://github.com/arvidn/libtorrent/blob/RC_1_1/include/libtorrent/string_util.hpp#L70

--- deluge/core/preferencesmanager.py
+++ deluge/core/preferencesmanager.py
@@ -231,7 +231,7 @@ class PreferencesManager(component.Component):
         self.core.apply_session_settings(
             {
                 'listen_system_port_fallback': self.config['listen_use_sys_port'],
-                'listen_interfaces': ''.join(interfaces),
+                'listen_interfaces': ','.join(interfaces),
             }
         )
 
