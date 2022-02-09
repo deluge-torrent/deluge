a1da2058bc830ff318c6cce6d453178c478bb20c
Chase Sterling <chase.sterling@gmail.com>
2022-02-09 16:04:10 -0500

    [Core] Enable file_completed_alert handling.
    Without adding file_progress to the alert mask, the
    TorrentFileCompletedEvent would never fire.
    
    Closes: https://dev.deluge-torrent.org/ticket/3421
    Closes: https://github.com/deluge-torrent/deluge/pull/370
    Ref: https://libtorrent.org/upgrade_to_1.2-ref.html

--- deluge/core/alertmanager.py
+++ deluge/core/alertmanager.py
@@ -46,6 +46,7 @@ class AlertManager(component.Component):
             | lt.alert.category_t.status_notification
             | lt.alert.category_t.ip_block_notification
             | lt.alert.category_t.performance_warning
+            | lt.alert.category_t.file_progress_notification
         )
 
         self.session.apply_settings({'alert_mask': alert_mask})
