2d3954a94637786089a44be0ddb8f2ee92f4c869
Calum Lind <calumlind+deluge@gmail.com>
2024-07-08 10:20:50 +0100

    Update nav header for new docs location

---
+++ deluge-torrent.org/htdocs/js/deluge_nav.js
@@ -0,0 +1,8 @@
+const navList = document.getElementById("deluge-nav-lists");
+function show_nav_menu() {
+  navList.classList.add("menus-show");
+}
+
+function hide_nav_menu() {
+  navList.classList.remove("menus-show");
+}
