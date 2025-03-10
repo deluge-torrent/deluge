deluge-torrent/deluge

commit d559f67ab976d869cf5a77122fc695f4619ea283

[Packaging] Fix pyinstaller to find installed deluge package data
Instead of relying on the source code paths use the pip installed Deluge
package data.

packaging/win/delugewin.spec
@@ -2,7 +2,7 @@
 import os
 import sys
 import deluge.common
-from PyInstaller.utils.hooks import collect_all, collect_submodules, copy_metadata
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules, copy_metadata
 
 datas = []
 binaries = []
@@ -100,8 +100,11 @@ for module in stdlib:
 hiddenimports += collect_submodules('twisted', filter=lambda name: 'test' not in name)
 datas += copy_metadata('twisted', recursive=True)
 
-# Copy UI/Plugin files to where pyinstaller expects
-datas += [('../../deluge/ui', 'deluge/ui'), ('../../deluge/plugins', 'deluge/plugins')]
+# Copy UI/Plugin and translation files to where pyinstaller expects
+package_data = collect_data_files('deluge')
+datas += package_data
+
+icon = [src for src, dest in package_data if src.endswith('deluge.ico')][0]
 
 # List of executables to produce
 executables = {
@@ -185,7 +188,7 @@ for e, d in executables.items():
         bootloader_ignore_signals=False,
         strip=False,
         upx=True,
-        icon='../../deluge/ui/data/pixmaps/deluge.ico',
+        icon=icon,
         console=d['console'],
         disable_windowed_traceback=False,
         target_arch=None,

commit 9f7392040fde96915fa722f792b71db27d2316af

[Packaging] deluge-console Windows workaround
Deluge-console broke on Windows after commit d559f67, which before that was working because it's source files getting included with rest UI/plugin data copied into place. This workaround here, copies back it's source files into place.

The underlying workarounded issues here is two-fold. It's not that anything is missing from the freezing, but rather that there at runtime is generated subparsers for the commands, using path_[0] (UI_PATH var in init.py, joined in console.py with rest path), so looking for files/paths not there anymore, as freezed now, and second, the use of 'overrides' decorator which breaks in freezed env since in decorators.py trying use inspect.stack()[2][4][0], and in freezed env inspect.stack()[2][4] is None, and hence any mention of former breaks with 'NoneType object is not subscriptable'. In non-freezed, it returns the call-context correctly, as source code readily available here.

packaging/win/delugewin.spec
@@ -107,6 +107,8 @@ datas += copy_metadata('twisted', recursive=True)
 # Copy UI/Plugin and translation files to where pyinstaller expects
 package_data = collect_data_files('deluge')
 datas += package_data
+# Workaround for Console UI files not being included in package data
+datas += [('../../deluge/ui/console', 'deluge/ui/console')]
 
 icon = [src for src, dest in package_data if src.endswith('deluge.ico')][0]
 