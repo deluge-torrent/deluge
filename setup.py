75f5d7bc8c789ab0c3936f6b8cab8cf5a8f4c99e
Daan Blom <contact@daanblom.com>
2026-02-27 16:00:47 +0800

    Impllemented changes requested by @cas--

--- setup.py
+++ setup.py
@@ -550,6 +550,7 @@ install_requires = [
     "pywin32; sys_platform == 'win32'",
     "certifi; sys_platform == 'win32'",
     'zope.interface',
+    "importlib_metadata; python_version < '3.8'",
 ]
 extras_require = {
     'all': [
